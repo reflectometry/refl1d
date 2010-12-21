ab8b569151c77c44adde54ad4a17b14618386c44
pkienzle <pkienzle@a92bf6fb-5b16-0410-9b54-b8becc4ab72c>
2010-12-21 15:19:01 +0000

    refl1d: temporarily suppress numpy errors on a routine
    git-svn-id: svn://danse.us/reflectometry/branches/pak2010@1508 a92bf6fb-5b16-0410-9b54-b8becc4ab72c

---
+++ refl1d/numpyerrors.py
@@ -0,0 +1,62 @@
+"""
+With context for handling numpy errors.
+
+Use this when you have a routine with numeric issues such as divide by zero
+which are known to be harmless, for example, because infinite or NaN results
+are allowed by the interface, or because the remainder of the code accommodates
+the exceptional conditions.
+
+Usage
+-----
+
+This is a wrapper around the numpy.seterr() command, and uses the same
+types of error handling controls, but in a with context or as a decorator::
+
+    with Errors(...):
+        statements
+
+    @errors(...)
+    def f(...):
+        statements
+
+A number of decorators are predefined: ignored, raised, printed, warned.
+
+Example
+-------
+
+    >>> import numpy
+    >>> with Errors(all='ignore'): 1/numpy.zeros(3)
+    >>> with Errors(all='print'): 1/numpy.zeros(3)
+    Warning: divide by zero encountered in divide
+    >>> @ignored
+    ... def f(): 1/numpy.zeros(3)
+    >>> f()
+    >>> @printed
+    ... def g(): 1/numpy.zeros(3)
+    >>> g()
+    Warning: divide by zero encounterd in divide
+"""
+from __future__ import with_statement
+import functools
+import numpy
+class Errors(object):
+    def __init__(self, **kw):
+        self.desired_state = kw
+    def __enter__(self):
+        self.current_state = numpy.geterr()
+        numpy.seterr(**self.desired_state)
+    def __exit__(self, *args):
+        numpy.seterr(**self.current_state)
+def errors(**kw):
+    context = Errors(**kw)
+    def decorator(f):
+        @functools.wraps(f)
+        def decorated(*args, **kw):            
+            with context: 
+                return f(*args, **kw)        
+        return decorated
+    return decorator
+ignored = errors(all='ignore')
+raised = errors(all='error')
+printed = errors(all='print')
+warned = errors(all='warn')
