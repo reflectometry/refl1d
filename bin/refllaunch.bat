e041fff0902aa4a556d4887e70b4442ec68ecd44
jkrycka <jkrycka@a92bf6fb-5b16-0410-9b54-b8becc4ab72c>
2011-01-04 19:45:38 +0000

    refl1d: update bat file to use --help instead of -help
    git-svn-id: svn://danse.us/reflectometry/branches/pak2010@1518 a92bf6fb-5b16-0410-9b54-b8becc4ab72c

--- bin/refllaunch.bat
+++ bin/refllaunch.bat
@@ -3,4 +3,4 @@
 rem On Windows XP, refllaunch.bat should be installed in a directory on the
 rem Path environment variable such as C:\WINDOWS\system32.
 
-cmd /k reflfit -help
+cmd /k reflfit --help
