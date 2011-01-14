79556f5a214460738abe7d9b145c10c190e603ee
pkienzle <pkienzle@a92bf6fb-5b16-0410-9b54-b8becc4ab72c>
2011-01-14 05:08:08 +0000

    refl1d: missing supporting checkins
    git-svn-id: svn://danse.us/reflectometry/trunk/refl1d@1538 a92bf6fb-5b16-0410-9b54-b8becc4ab72c

---
+++ refl1d/support.py
@@ -0,0 +1,42 @@
+"""
+Support files for the application.
+
+This includes tools to help with testing, documentation, command line
+parsing, etc. which are specific to this application, rather than general
+utilities.
+"""
+import os
+
+def get_data_path():
+    """
+    Locate the examples directory.
+    """
+
+    # Check for data path in the environment
+    key = 'REFLECTOMETRY_DATA'
+    if os.environ.has_key(key):
+        path = os.path.join(os.environ[key],data)
+        if not os.path.isdir(path):
+            raise RuntimeError('Path in environment %s not a directory'%key)
+        return path
+
+    # Check for data next to the package.
+    try:
+        root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        return os.path.join(root,'examples')
+    except:
+        raise RuntimeError("Could not find sample data")
+
+
+REGISTRY = { 
+    'exp123.dat': ['xray','e1085009.log'],
+    'chale207.refl': ['polymer','10ndt001.refl'],
+    '10ndt001.refl': ['polymer','10ndt001.refl'],
+    'lha03_255G.refl': ['spinvalve','n101Gc1.refl'],
+    }
+def sample_data(file):
+    examples = get_data_path()
+    if file in REGISTRY:
+        return os.path.join(examples, *REGISTRY[file])
+    else:
+        raise ValueError("Sample dataset %s not available"%file)
