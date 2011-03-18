36bfca5daf8dbb8cce63e2bcfafd6a4c8427f759
pkienzle <pkienzle@a92bf6fb-5b16-0410-9b54-b8becc4ab72c>
2011-03-18 21:59:14 +0000

    refl1d: use multiprocessing rather than threads in optimizer analysis
    git-svn-id: svn://danse.us/reflectometry/trunk/refl1d@1657 a92bf6fb-5b16-0410-9b54-b8becc4ab72c

--- compareopt/compare.py
+++ compareopt/compare.py
@@ -1,46 +1,46 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
 """
 Optimizer performance comparison
 """
 import os
 import sys
-import Queue
-import threading
+import subprocess
+from multiprocessing import Pool
 
 root = 'out'
 
-problems = [
-    '../examples/xray/model.py', 
-    ]
+problems = {
+    'xray': '../examples/xray/model.py',
+    }
 
-fitters = [
-    ['--fit=de'],
-#    ['--fit=rl'],
-    ]
+fitters = {
+    'de': '--fit=de --steps=50',
+    'rl': '--fit=rl --steps=50',
+    }
 
-nthread = 8
-nrepeat = 30
+nthread = None # Use #cpus
+nrepeat = 3
 
-def run(queue, root):
-    while True:
-        i,j,pi,fj = queue.get()
-        for k in range(repeat):
-            dir = os.path.join(root, "P"+str(i), "F"+str(j), "T"+str(k))
-            os.makedirs(dir)
-            cmd = "../bin/refl1d --batch --stepmon --overwrite --random --store=%s '%s' %s"%(dir, pi, " ".join(fj))
-            os.system(cmd)
-        queue.task_done()
+def cmd(store,model,opts):
+    pat = "nice ../bin/refl1d --batch --stepmon --overwrite --random --store=%s '%s' %s"
+    return pat%(store,model,opts)
+
+def run(job):
+    modelname,model,fitname,opts,k = job
+    store = os.path.join(root, modelname, fitname, "T%d"%k)
+    if not os.path.exists(store):
+        os.makedirs(store)
+    print "starting %d %s %s\n"%(k,model,opts),
+    subprocess.call(cmd(store,model,opts), shell=True)
 
 def main(problems, fitters, nthread, root):
-    queue = Queue()
-    for _ in range(nthread):
-        t = Thread(target=run, args=(queue,root))
-        t.daemon = True
-        t.start()
-    for i, pi in enumerate(problems):
-        for j, fj in enumerate(fitters):
-            queue.add((i,j,pi,fj))
-    queue.join()
-    
-if __name__ == "__main__": 
+    if os.path.exists(root):
+        raise RuntimeError("directory %s exists."%root)
+    pool = Pool(processes=nthread)
+    pool.map(run, [(i,pi,j,fj,k)
+                   for i,pi in problems.items()
+                   for j,fj in fitters.items()
+                   for k in range(nrepeat)] )
+
+if __name__ == "__main__":
     main(problems, fitters, nthread, root)
