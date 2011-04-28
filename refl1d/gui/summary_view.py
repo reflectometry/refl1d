242dc63a830af7bd9638dd58336b11528ab0881b
jkrycka <jkrycka@a92bf6fb-5b16-0410-9b54-b8becc4ab72c>
2011-04-28 17:18:44 +0000

    refl1d gui: correct previous checkin as wrong module was used
    git-svn-id: svn://danse.us/reflectometry/trunk/refl1d@1764 a92bf6fb-5b16-0410-9b54-b8becc4ab72c

--- refl1d/gui/summary_view.py
+++ refl1d/gui/summary_view.py
@@ -2,9 +2,8 @@ from __future__ import division
 import wx
 
 import  wx.lib.scrolledpanel as scrolled
-from wx.lib.pubsub import Publisher as pub
 
-from .util import nice
+from .util import nice, publish, subscribe
 
 class SummaryView(scrolled.ScrolledPanel):
     """
@@ -15,12 +14,8 @@ class SummaryView(scrolled.ScrolledPanel):
         scrolled.ScrolledPanel.__init__(self, parent, -1)
         self.parent = parent
 
-        # set new model
-        pub.subscribe(self.OnInitialModel, "initial_model")
-        # change model structure message
-        pub.subscribe(self.OnUpdateModel, "update_model")
-        # change model parameter message
-        pub.subscribe(self.OnUpdateParameters, "update_parameters")
+        subscribe(self.OnModelChange, "model.change")
+        subscribe(self.OnModelUpdate, "model.update")
 
         # event for showing notebook tab when it is clicked
         self.Bind(wx.EVT_SHOW, self.OnShow)
@@ -33,22 +28,19 @@ class SummaryView(scrolled.ScrolledPanel):
         self.SetupScrolling()
 
     # ============= Signal bindings =========================
-    def OnInitialModel(self, event):
-        self.set_model(event.data)
-
-    def OnUpdateModel(self, event):
-        if self.model == event.data:
-            self.update_model()
-
-    def OnUpdateParameters(self, event):
-        if self.model == event.data:
-            self.update_parameters()
-
     def OnShow(self, event):
         if self._reset_model:
            self.update_model()
         elif self._reset_parameters:
            self.update_parameters()
+
+    def OnModelChange(self, model):
+        if self.model == model:
+            self.update_model()
+
+    def OnModelUpdate(self, model):
+        if self.model == model:
+            self.update_parameters()
 
     # ============ Operations on the model  ===============
 
@@ -68,13 +60,13 @@ class SummaryView(scrolled.ScrolledPanel):
         vbox = wx.BoxSizer(wx.VERTICAL)
         hbox = wx.BoxSizer(wx.HORIZONTAL)
         bagSizer = wx.GridBagSizer(hgap=3, vgap=5)
-                
+
         self.layer_label = wx.StaticText(self, wx.ID_ANY, 'Layer Name')
         self.slider_label = wx.StaticText(self, wx.ID_ANY, '')
         self.value_label = wx.StaticText(self, wx.ID_ANY, 'Value')
         self.low_label = wx.StaticText(self, wx.ID_ANY, 'Low Range')
         self.high_label = wx.StaticText(self, wx.ID_ANY, 'High Range')
-        
+
         hbox.Add(self.layer_label, 1, wx.LEFT, 1)
         hbox.Add(self.slider_label, 1, wx.EXPAND|wx.LEFT, 112)
         hbox.Add(self.value_label, 1, wx.EXPAND|wx.LEFT, 35)
@@ -82,11 +74,11 @@ class SummaryView(scrolled.ScrolledPanel):
         hbox.Add(self.high_label, 1, wx.EXPAND|wx.LEFT, 31)
 
         bagSizer.Add(hbox,pos=(0,0))
-        
+
         line = wx.StaticLine(self, -1 )
 
         bagSizer.Add(line, pos=(1,0),flag=wx.EXPAND|wx.RIGHT, border=5)
-                
+
         self.output = []
 
         for p in sorted(self.model.parameters, cmp=lambda x,y: cmp(x.name,y.name)):
@@ -97,7 +89,8 @@ class SummaryView(scrolled.ScrolledPanel):
 
         vbox.Add(bagSizer)
         self.SetSizerAndFit(vbox)
-        
+
+
     def update_parameters(self):
         if not self.IsShown():
             self._reset_parameters = True
@@ -122,8 +115,8 @@ class ParameterSummary(wx.Panel):
         self.layer_name = wx.StaticText(self, wx.ID_ANY,
                           str(self.parameter.name), style=wx.TE_LEFT)
         self.slider = wx.Slider(self, -1, 0, 0, 100,
-                      size=(100, 18), style=wx.SL_AUTOTICKS|wx.SL_HORIZONTAL)
-        #self.slider.SetThumbLength(6)
+                      size=(100, 8), style=wx.SL_AUTOTICKS|wx.SL_HORIZONTAL)
+        self.slider.SetThumbLength(3)
         self.value = wx.StaticText(self, wx.ID_ANY, str(self.parameter.value),
                      style=wx.TE_LEFT )
         self.min_range = wx.StaticText(self, wx.ID_ANY, str(self.low),
@@ -164,5 +157,4 @@ class ParameterSummary(wx.Panel):
         new_value  = self.parameter.bounds.put01(value/100)
         self.parameter.value = new_value
         self.value.SetLabel(str(nice(new_value)))
-        pub.sendMessage("update_parameters", self.model)
-
+        publish("model.update", model=self.model)
