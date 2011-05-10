d7cae2792e7f17a4a4f74d33e0003b368a97e375
pkienzle <pkienzle@a92bf6fb-5b16-0410-9b54-b8becc4ab72c>
2011-05-10 05:20:56 +0000

    refl1d: missing signal.py added
    git-svn-id: svn://danse.us/reflectometry/trunk/refl1d@1857 a92bf6fb-5b16-0410-9b54-b8becc4ab72c

---
+++ refl1d/gui/signal.py
@@ -0,0 +1,30 @@
+import wx
+from wx.py.dispatcher import send, connect
+
+def model_new(model):
+    send('model.new',model=model)
+
+def update_model(model, dirty=True):
+    # signaller is responsible for marking the model as needing recalculation
+    if dirty: model.model_update()
+    send('model.update_structure',model=model)
+
+_DELAYED_SIGNAL = {}
+def update_parameters(model, delay=50):
+    """
+    Inform all views that the model has changed.  Note that if the model
+    is changing rapidly, then the signal will be delayed for a time.
+    """
+    # signaller is responsible for marking the model as needing recalculation
+    try:
+        _DELAYED_SIGNAL[model].Restart(delay)
+    except:
+        def _send_signal():
+            #print "sending update parameters",model
+            del _DELAYED_SIGNAL[model]
+            model.model_update()
+            send('model.update_parameters',model=model)
+        _DELAYED_SIGNAL[model] = wx.FutureCall(delay, _send_signal)
+
+def log_message(message):
+    send('log',message=message)
