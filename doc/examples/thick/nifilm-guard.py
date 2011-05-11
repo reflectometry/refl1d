8d165cbd9ad42568719ddf27a3c149744b2dc1ae
pkienzle <pkienzle@a92bf6fb-5b16-0410-9b54-b8becc4ab72c>
2011-05-11 18:20:33 +0000

    refl1d: thick layer example
    git-svn-id: svn://danse.us/reflectometry/trunk/refl1d@1869 a92bf6fb-5b16-0410-9b54-b8becc4ab72c

---
+++ doc/examples/thick/nifilm-guard.py
@@ -0,0 +1,18 @@
+from refl1d.names import *
+
+nickel = Material('Ni')
+
+#nickel = SLD(rho=9.4)
+
+sample = silicon(0,5) | nickel(10000,5) | air
+
+T = numpy.linspace(0, 5, 100)
+
+probe = NeutronProbe(T=T, dT=0.01, L=4.75, dL=0.0475)
+probe.oversample(n=100)
+
+M = Experiment(probe=probe, sample=sample)
+
+#M.simulate_data(5)
+
+problem = FitProblem(M)
