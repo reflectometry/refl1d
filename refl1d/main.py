27cd839d54a2f43c96e8b0f934d4924993ddebff
Paul Kienzle <pkienzle@nist.gov>
2014-05-29 11:35:46 -0400

    move matplotlib support routine from refl1d to bumps

--- refl1d/main.py
+++ refl1d/main.py
@@ -3,23 +3,13 @@
 Run bumps with refl1d plugin
 """
 
-def set_mplconfig():
-    """
-    Point the matplotlib config dir to somewhere writeable
-    """
-    import os,sys
-    if hasattr(sys, 'frozen'):
-        mplconfigdir = os.path.join(os.environ['LOCALAPPDATA'],'Refl1D', 'mplconfig')
-        mplconfigdir = os.environ.setdefault('MPLCONFIGDIR',mplconfigdir)
-        if not os.path.exists(mplconfigdir): os.makedirs(mplconfigdir)
-
 def setup_bumps():
     """
     Install the refl1d plugin into bumps, but don't run main.
     """
-    set_mplconfig()
-    from . import fitplugin
     import bumps.cli
+    bumps.cli.set_mplconfig(appdatadir='Refl1D')
+    from . import fitplugin
     bumps.cli.install_plugin(fitplugin)
 
 def cli():
