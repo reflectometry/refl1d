3df6efae20cbd5f37eff473363711e6d8cf7015a
Paul Kienzle <pkienzle@nist.gov>
2015-09-14 12:47:12 -0400

    fix start/end values from profile in neighbouring layers

--- doc/examples/profile/model.py
+++ doc/examples/profile/model.py
@@ -75,10 +75,20 @@ def mag(z, z1, z2, M1, M2, M3):
         return ret
     return v2()
 
+flayer = FP(100, 0, name="sin", profile=nuc, period=10, phase=0.2,
+            magnetism=FM(profile=mag, M1=1, M2=4, M3=5, z1=10, z2=40))
+
 sample = (silicon(0,5)
-          | FP(100,0,name="sin",profile=nuc, period=10, phase=0.2,
-               magnetism=FM(profile=mag, M1=1, M2=4, M3=5, z1=10, z2=40))
+          | flayer
+          | flayer.end(35, 15, magnetism=flayer.magnetism.end)
           | air)
+
+# Need to be able to compute the thickness of the functional magnetic
+# layer, which unfortunately requires the layer stack and the index
+# into the layer stack, which can be the layer number in the stack,
+# the layer name, or if there are multiple layers with the same name,
+# the (layer name, k), where the magnetism is attached to the kth layer.
+flayer.magnetism.set_anchor(sample, 'sin')
 
 sample['sin'].period.range(0,100)
 sample['sin'].phase.range(0,1)
