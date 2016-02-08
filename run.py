65bb8bc422a04962c113f72cff78f86cf8f5831e
Paul Kienzle <pkienzle@nist.gov>
2016-02-08 10:51:00 -0500

    test against python 2.6

--- run.py
+++ run.py
@@ -6,14 +6,16 @@ Usage:
 
 ./run.py [refl1d cli args]
 """
-import os, sys
+import os
+import sys
+from os.path import abspath, join as joinpath, dirname
 
 def addpath(path):
     """
     Add a directory to the python path environment, and to the PYTHONPATH
     environment variable for subprocesses.
     """
-    path = os.path.abspath(path)
+    path = abspath(path)
     if 'PYTHONPATH' in os.environ:
         PYTHONPATH = path + os.pathsep + os.environ['PYTHONPATH']
     else:
@@ -31,17 +33,22 @@ def cd(path):
 
 def import_dll(module, build_path):
     """Import a DLL from the build directory"""
-    import sysconfig, imp
-    ext = sysconfig.get_config_var('SO')
+    import imp
+    try:
+        import sysconfig
+        ext = sysconfig.get_config_var('SO')
+    except ImportError:
+        ext = '.pyd' if sys.platform == 'win32' else '.so'
+
     # build_path comes from context
-    path = os.path.join(build_path, *module.split('.'))+ext
-    #print(" ".join(("importing", module, "from", path)))
+    path = joinpath(build_path, *module.split('.'))+ext
+    print(" ".join(("importing", module, "from", path)))
     mod = imp.load_dynamic(module, path)
 
     # make sure module can be imported from package
-    package_name,module_name = module.rsplit('.',1)
+    package_name, module_name = module.rsplit('.', 1)
     package = __import__(package_name)
-    setattr(package,module_name,mod)
+    setattr(package, module_name, mod)
     return mod
 
 def prepare_environment():
@@ -51,16 +58,16 @@ def prepare_environment():
     #sys.dont_write_bytecode = True
 
     # Make sure that we have a private version of mplconfig
-    #mplconfig = os.path.join(os.getcwd(), '.mplconfig')
+    #mplconfig = joinpath(os.getcwd(), '.mplconfig')
     #os.environ['MPLCONFIGDIR'] = mplconfig
     #if not os.path.exists(mplconfig):
     #    os.mkdir(mplconfig)
 
     #import numpy; numpy.seterr(all='raise')
-    refl1d_root = os.path.abspath(os.path.dirname(__file__))
-    periodictable_root = os.path.abspath(os.path.join(refl1d_root, '..','periodictable'))
-    bumps_root = os.path.abspath(os.path.join(refl1d_root, '..','bumps'))
-    refl1d_build = os.path.join(refl1d_root,'build','lib'+platform)
+    refl1d_root = abspath(dirname(__file__))
+    periodictable_root = abspath(joinpath(refl1d_root, '..', 'periodictable'))
+    bumps_root = abspath(joinpath(refl1d_root, '..', 'bumps'))
+    refl1d_build = joinpath(refl1d_root, 'build', 'lib'+platform)
 
     # add bumps and periodictable to the path
     addpath(periodictable_root)
@@ -70,7 +77,8 @@ def prepare_environment():
     import subprocess
     with open(os.devnull, 'w') as devnull:
         with cd(refl1d_root):
-            subprocess.call((sys.executable, "setup.py", "build"), shell=False, stdout=devnull)
+            subprocess.call((sys.executable, "setup.py", "build"),
+                            shell=False, stdout=devnull)
 
     # Add the build dir to the system path
     #addpath(refl1d_build)
@@ -79,7 +87,7 @@ def prepare_environment():
     import_dll('refl1d.calc_g_zs_cex', refl1d_build)
 
     # Make sample data and models available
-    os.environ['REFL1D_DATA'] = os.path.join(refl1d_root,'doc','_examples')
+    os.environ['REFL1D_DATA'] = joinpath(refl1d_root, 'doc', '_examples')
 
     #print "\n".join(sys.path)
 
