5f5728b8e966f781e1e077ac96819ed87513ecee
Paul Kienzle <pkienzle@nist.gov>
2017-11-09 16:28:14 -0500

    add magnetic roughness model

---
+++ doc/examples/magrough/model.py
@@ -0,0 +1,48 @@
+from refl1d.names import *
+nickel = Material('Ni')
+copper = Material('Cu')
+
+#nickel = SLD(rho=9.4)
+
+s_mid = Parameter(name='sigma mid', value=5)
+s_low = s_mid/5
+s_high = s_mid*3
+interface_below, interface_above = s_mid, s_mid
+#interface_below, interface_above = s_high, s_low
+#interface_below, interface_above = s_low, s_high
+#thetaM, thetaM2 = 90., 270.   # spin flip
+thetaM, thetaM2 = 90., 90.   # non spin flip
+sample = (
+    silicon(0, s_mid)
+    | nickel(100, s_mid, magnetism=Magnetism(rhoM=5, thetaM=thetaM,
+             interface_below=interface_below, interface_above=interface_above))
+    | copper(10, 5)
+    | nickel(100, 5, magnetism=Magnetism(rhoM=5, thetaM=thetaM2))
+    | copper(10, 5)
+    | air
+)
+del sample[2:5]
+
+T = numpy.linspace(0, 5, 400)
+dT,L,dL = 0.02,4.75, 0.0475
+
+xs = [NeutronProbe(T=T, dT=0.01, L=4.75, dL=0.0475, name=name)
+      for name in ("Nevot-Croce", "fine", "coarse")]
+# spin flip/non spin flip
+if thetaM != thetaM2:
+    probes = [PolarizedNeutronProbe([v,v,v,v], H=0, Aguide=270) for v in xs]
+else:
+    probes = [PolarizedNeutronProbe([v,None,None,v], H=0, Aguide=270) for v in xs]
+
+nc, fine, coarse = probes
+
+M1 = Experiment(probe=nc, sample=sample, name="Nevot-Croce")
+M2 = Experiment(probe=fine, sample=sample, name="fine step",
+                step_interfaces=True, dz=0.1)
+M3 = Experiment(probe=coarse, sample=sample, name="coarse step",
+                step_interfaces=True, dz=2)
+
+#M1.simulate_data(0.05)
+#M2.simulate_data(0.05)
+
+problem = MultiFitProblem([M1,M2])
