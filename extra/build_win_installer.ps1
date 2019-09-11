29871973a91f3dae4a04ea90991c19763ef8deb5
Paul Kienzle <pkienzle@gmail.com>
2019-09-10 23:23:55 -0400

    use local wheel for win installer; generalize and document installer builder

--- extra/build_win_installer.ps1
+++ extra/build_win_installer.ps1
@@ -1,16 +1,38 @@
+# Install the application in the embedded python for windows distribution.
+# This script fetches the embedded distribution from python.org and installs
+# pip. It then uses pip to install the binary wheel from the dist directory.
+# Remove dist\*.whl and run "pythoon setup.py bdist_wheel" before running.
+
+# TODO: Use the PYTHON environment variable to infer the version of python.
+$PY_VERSION = "3.7.4"
+$PTH_FILE = "python37._pth"
+$WHEEL_TAG = "cp37-cp37m-win_amd64"
+$INSTALL_TAG = "cp37m-embedded-amd64"
+$PACKAGE = "refl1d"
+$APP_NAME = "Refl1D"
+
+# Grab version number from the application source.  Assumes it is in the
+$APP_VERSION = (Select-String '__version__ *= *"(.*)"' "$PACKAGE\__init__.py" -ca).matches | Select-Object -ExpandProperty Groups | Select-Object -Last 1 -ExpandProperty value
+
+# Fetch embedded python zip file
 $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
 [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
-Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.4/python-3.7.4-embed-amd64.zip" -OutFile python_embedded.zip
+Invoke-WebRequest -Uri "https://www.python.org/ftp/python/$PY_VERSION/python-$PY_VERSION-embed-amd64.zip" -OutFile python_embedded.zip
 Invoke-WebRequest -Uri "https://bootstrap.pypa.io/get-pip.py" -OutFile get-pip.py
 
-Expand-Archive -path '.\python_embedded.zip' -destinationpath 'build\Refl1D'
-cd 'build\Refl1D'
+# Expand the zip file into the build directory where it won't collide with the
+# package source directory.  Note: The directory name $APP_NAME will appear on
+# the users machine when they expand the zipped distribution we are creating.
+Expand-Archive -path '.\python_embedded.zip' -destinationpath "build\$APP_NAME"
 
-(Get-Content python37._pth).replace('#import site', 'import site') | Set-Content python37._pth
-.\python.exe ..\..\get-pip.py
-.\Scripts\pip.exe install Refl1D
+# Enable pip in the embedded python distribution
+cd "build\$APP_NAME"
+(Get-Content $PTH_FILE).replace('#import site', 'import site') | Set-Content $PTH_FILE
+.\python.exe "..\..\get-pip.py"
 
+# Use pip to install the app from the binary wheel in the dist directory.
 cd ..
-$VERSION = (Select-String '__version__ *= *"(.*)"' ..\refl1d\__init__.py -ca).matches | Select-Object -ExpandProperty Groups | Select-Object -Last 1 -ExpandProperty value
-$PYVERSION = "-cp37m-embedded-amd64"
-Compress-Archive -Path Refl1D -DestinationPath "..\dist\Refl1D-$VERSION$PYVERSION.zip"
+"$APP_NAME\Scripts\pip.exe" install "..\dist\$PACKAGE-$APP_VERSION-$WHEEL_TAG.whl"
+
+# Create the embedded app archive in the dist directory .
+Compress-Archive -Path "$APP_NAME" -DestinationPath "..\dist\$APP_NAME-$APP_VERSION-$INSTALL_TAG.zip"
