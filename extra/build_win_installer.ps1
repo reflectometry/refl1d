f23d22c00b247488b64a71fddbba6aa2fc941a64
Paul Kienzle <pkienzle@nist.gov>
2020-01-02 19:40:06 -0500

    appveyor app build triggered; try to fix

--- extra/build_win_installer.ps1
+++ extra/build_win_installer.ps1
@@ -12,10 +12,13 @@ $INSTALL_TAG = "cp38m-embedded-amd64"
 $PACKAGE = "refl1d"
 $APP_NAME = "Refl1D"
 
-# Grab version number from the application source.  Assumes it is in the
+# Grab version number from the application source.  Assumes it is defined as
+#     package\__init__.py:__version__ = "..."
 $APP_VERSION = (Select-String '__version__ *= *"(.*)"' "$PACKAGE\__init__.py" -ca).matches | Select-Object -ExpandProperty Groups | Select-Object -Last 1 -ExpandProperty value
+Write-Host "$APP_NAME: $APP_VERSION"
 
 # Fetch embedded python zip file
+Write-Host "Fetching python"
 $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
 [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
 Invoke-WebRequest -Uri "https://www.python.org/ftp/python/$PY_VERSION/python-$PY_VERSION-embed-amd64.zip" -OutFile python_embedded.zip
@@ -24,16 +27,22 @@ Invoke-WebRequest -Uri "https://bootstrap.pypa.io/get-pip.py" -OutFile get-pip.p
 # Expand the zip file into the build directory where it won't collide with the
 # package source directory.  Note: The directory name $APP_NAME will appear on
 # the users machine when they expand the zipped distribution we are creating.
-Expand-Archive -path '.\python_embedded.zip' -destinationpath "build\$APP_NAME"
+Write-Host "Preparing embedded environment"
+Expand-Archive -path 'python_embedded.zip' -destinationpath "build\$APP_NAME"
 
 # Enable pip in the embedded python distribution
-cd "build\$APP_NAME"
-(Get-Content $PTH_FILE).replace('#import site', 'import site') | Set-Content $PTH_FILE
-.\python.exe "..\..\get-pip.py"
+$PTH_PATH = "build\$APP_NAME\$PTH_FILE"
+(Get-Content "$PTH_PATH").replace('#import site', 'import site') | Set-Content "$PTH_PATH"
+& "build\$APP_NAME\python.exe" "get-pip.py"
 
 # Use pip to install the app from the binary wheel in the dist directory.
-cd ..
-& "$APP_NAME\Scripts\pip.exe" install "$PSScriptRoot\dist\$PACKAGE-$APP_VERSION-$WHEEL_TAG.whl"
+Write-Host "Installing $APP_NAME-$APP_VERSION into embedded environment"
+& "build\$APP_NAME\Scripts\pip.exe" install --no-warn-script-location "$PSScriptRoot\..\dist\$PACKAGE-$APP_VERSION-$WHEEL_TAG.whl"
 
 # Create the embedded app archive in the dist directory .
-Compress-Archive -Path "$APP_NAME" -DestinationPath "$PSScriptRoot\dist\$APP_NAME-$APP_VERSION-$INSTALL_TAG.zip"
+Write-Host "Compressing into $ZIP_PATH"
+cd build
+$ZIP_PATH = "dist\$APP_NAME-$APP_VERSION-$INSTALL_TAG.zip"
+Compress-Archive -Path "$APP_NAME" -DestinationPath "$PSScriptRoot\..\$ZIP_PATH"
+
+Write-Host "All done!!"
