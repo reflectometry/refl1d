05c9f26b7f18f8a48b192c1b469b3c288b8933c1
Brian Ben Maranville <brian.maranville@nist.gov>
2021-05-03 15:49:27 -0400

    need to avoid directly inheriting bumps.fitproblem.FitProblem in schema, since it implements models as a property

--- refl1d/fitproblem.py
+++ refl1d/fitproblem.py
@@ -1,12 +1,15 @@
 from bumps.util import schema, List
-from bumps.fitproblem import FitProblem as _FitProblem
+from bumps.fitproblem import FitProblem as _FitProblem, FitProblemSchema as _FitProblemSchema
 from .experiment import Experiment
 
 # @schema(eq=False, init=False)
 # class BaseFitProblem(_BaseFitProblem):
 #     fitness: Experiment
 
-@schema(eq=False, init=False)
-class FitProblem(_FitProblem):
+@schema(classname="FitProblem", eq=False, init=False)
+class FitProblemSchema(_FitProblemSchema):
     __doc__ =  _FitProblem.__doc__.replace("Fitness", "Experiment")
     models: List[Experiment]
+
+class FitProblem(_FitProblem):
+    ...
