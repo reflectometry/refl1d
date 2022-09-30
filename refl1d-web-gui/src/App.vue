6d16a2beb8789bd397c486163b961698b635d2c3
bbm <brian.maranville@nist.gov>
2022-09-30 07:07:29 -0400

    move component-specific listener registration on socket to components, add fileBrowser

--- refl1d-web-gui/src/App.vue
+++ refl1d-web-gui/src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { Button } from 'bootstrap/dist/js/bootstrap.esm.js';
-import { onMounted, ref, shallowRef } from 'vue';
+import { onMounted, ref, shallowRef, VNodeRef } from 'vue';
 import { api } from './server_api';
 import { io, Socket } from 'socket.io-client';
 import FitOptions from './components/FitOptions.vue';
+import DataView from './components/DataView.vue';
+import ModelView from './components/ModelView.vue';
+import PanelTabContainer from './components/PanelTabContainer.vue';
+import FileBrowser from './components/FileBrowser.vue';
+import SummaryView from './components/SummaryView.vue';
 // import { FITTERS as FITTER_DEFAULTS } from './fitter_defaults';
-const FITTER_DEFAULTS = {};
 
 const REFLECTIVITY_PLOTS = [
   "Fresnel",
@@ -19,73 +23,71 @@ type ReflectivityPlotEnum = typeof REFLECTIVITY_PLOTS;
 type ReflectivityPlot = ReflectivityPlotEnum[number];
 
 const reflectivity_type = ref<ReflectivityPlot>("Linear");
-const connection_id = ref("");
-const connection = ref<Socket | null>(null);
 const connected = ref(false);
-const menuToggle = ref(null);
-const fitOptions = ref(null);
-const fitter_settings = shallowRef(structuredClone(FITTER_DEFAULTS));
-const fitter_defaults = shallowRef(FITTER_DEFAULTS);
-const active_fitter = ref('amoeba');
+const menuToggle = ref<HTMLButtonElement>();
+const fitOptions = ref<typeof FitOptions>();
+const fileBrowser = ref<typeof FileBrowser>();
+const fileBrowserSelectCallback = ref((pathlist: string[], filename: string) =>  {});
+const file_picker = ref<HTMLInputElement>();
 
 function set_reflectivity(refl_type: ReflectivityPlot) {
   reflectivity_type.value = refl_type;
 }
 
-function load_model_path(path: string) {
+// Create a SocketIO connection, to be passed to child components
+// so that they can do their own communications with the host.
 
-}
+const socket = io('', {
+  // this is mostly here to test what happens on server fail:
+  reconnectionAttempts: 10
+});
+socket.on('connect', () => {
+  console.log(socket.id);
+  connected.value = true;
+});
 
-function connect() {
-  const socket = io('http://localhost:8080', {
-    reconnectionAttempts: 10
-  });
-  connection.value = socket;
-  socket.on('connect', () => {
-    console.log(socket.id);
-    connected.value = true;
-  });
-
-  socket.on('fitter-settings', (payload) => {
-    fitter_settings.value = payload;
-  });
-  socket.on('fitter-defaults', (payload) => {
-    fitter_defaults.value = payload;
-  });
-  socket.on('fitter-active', (payload) => {
-    active_fitter.value = payload;
-  });
-  socket.on('disconnect', (payload) => {
-    console.log("disconnected!", payload);
-    connected.value = false;
-  })
-}
+socket.on('disconnect', (payload) => {
+  console.log("disconnected!", payload);
+  // connected.value = false;
+})
 
 function disconnect() {
-  connection.value?.disconnect();
+  socket.disconnect();
   connected.value = false;
 }
 
+function selectOpenFile() {
+  if (fileBrowser.value) {
+    fileBrowserSelectCallback.value = (pathlist, filename) => {
+      socket.emit("load_model_file", pathlist, filename);
+    }
+    fileBrowser.value.open();
+  }
+  // const path = prompt("full path to file:");
+  // if (path != null) {
+  //   let defaulted_path = (path == '') ? '/home/bbm/dev/refl1d-modelbuilder/ISIS_GGG/GGG_GdIG_MultiFit.py' : path;
+  //   socket.emit("load_model_file", defaulted_path);
+  // }
+  // file_picker.value?.click();
+}
+
+function load_model_path(event: Event) {
+  console.log(event);
+  const { files } = event.target;
+  console.log(files);
+  // socket.emit("load_model_file", files[0]);
+  window.testevent = event;
+}
+
+
 function openFitOptions() {
-  fitOptions?.value?.open();
-}
-
-function get_sessions() {
-  fetch('http://localhost:8080/get_sessions');
-}
-
-function updateActiveFitter(fitter_name) {
-  connection.value?.emit("fitter-active", fitter_name);
-}
-
-function updateActiveSettings(new_settings) {
-  connection.value?.emit("fitter-settings", new_settings);
+  fitOptions.value?.open();
 }
 
 onMounted(() => {
   const menuToggleButton = new Button(menuToggle.value);
-  connect();
 });
+
 </script>
 
 <template>
@@ -103,7 +105,7 @@ onMounted(() => {
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item dropdown">
+            <!-- <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                 aria-expanded="false">
                 Session
@@ -113,14 +115,14 @@ onMounted(() => {
                 <li><a class="dropdown-item" href="#" @click="disconnect">Disconnect</a></li>
                 <li><a class="dropdown-item" href="#" @click="reconnect">Existing</a></li>
               </ul>
-            </li>
+            </li> -->
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                 aria-expanded="false">
                 File
               </a>
               <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Open</a></li>
+                <li><a class="dropdown-item" href="#" @click="selectOpenFile">Open</a></li>
                 <li><a class="dropdown-item" href="#">Save</a></li>
                 <li>
                   <hr class="dropdown-divider">
@@ -142,7 +144,7 @@ onMounted(() => {
                 <li><a class="dropdown-item" href="#" @click="openFitOptions">Options...</a></li>
               </ul>
             </li>
-            <li class="nav-item dropdown">
+            <!-- <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                 aria-expanded="false">
                 Reflectivity
@@ -153,22 +155,29 @@ onMounted(() => {
                     @click="set_reflectivity(plot_type)">{{plot_type}}</a>
                 </li>
               </ul>
-            </li>
+            </li> -->
           </ul>
           <div class="d-flex">
-            <div id="connection_status" :class="{'btn': true, 'btn-outline-success': connected, 'btn-outline-danger': !connected}">
+            <div id="connection_status"
+              :class="{'btn': true, 'btn-outline-success': connected, 'btn-outline-danger': !connected}">
               {{(connected) ? 'connected' : 'disconnected'}}</div>
 
           </div>
         </div>
       </div>
     </nav>
+    <PanelTabContainer :panels="[DataView, ModelView, SummaryView]" :socket="socket" />
   </div>
-  <FitOptions ref="fitOptions" :fitter_defaults="fitter_defaults" :fitter_settings="fitter_settings"
-    :active_fitter="active_fitter" @active-fitter="updateActiveFitter" @active-settings="updateActiveSettings"/>
+  <FitOptions ref="fitOptions" :socket="socket" />
+  <FileBrowser ref="fileBrowser" :socket="socket" title="Load Model File" :callback="fileBrowserSelectCallback" />
+  <input ref="file_picker" type="file" multiple="false" id="file_picker" style="display:none;" @change="load_model_path" />
 </template>
 
-<style scoped>
+<style>
+html, body, #app {
+  height: 100%;
+}
+
 div#connection_status {
   pointer-events: none;
 }
