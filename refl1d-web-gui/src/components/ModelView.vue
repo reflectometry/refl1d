425a9ed73e3ed750de15735f72b6333b5c2c14ef
bbm <brian.maranville@nist.gov>
2022-10-03 16:36:57 -0400

    add animation loop, only call for new profile plot after finished retrieving one

--- refl1d-web-gui/src/components/ModelView.vue
+++ refl1d-web-gui/src/components/ModelView.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { ref, onMounted, watch, onUpdated, computed, shallowRef } from 'vue';
-import type { Socket } from 'socket.io-client';
+import { Socket } from 'socket.io-client';
 import mpld3 from 'mpld3';
 
 const title = "Profile";
 const plot_div = ref<HTMLDivElement>();
+const draw_requested = ref(false);
 
 const props = defineProps<{
   socket: Socket,
@@ -12,24 +13,36 @@ const props = defineProps<{
 }>();
 
 onMounted(() => {
-  props.socket.on('plot_update_ready', () => {
+  props.socket.on('plot_update_ready', ({ message, timestamp }) => {
     if (props.visible) {
-      fetch_and_draw();
+      draw_requested.value = true;
     }
   });
+  window.requestAnimationFrame(draw_if_needed);
 });
 
 function fetch_and_draw() {
   props.socket.emit('get_profile_plot', (payload) => {
-    console.log(payload, plot_div.value);
-    payload.width = plot_div.value?.clientWidth ?? 640;
-    payload.height = plot_div.value?.clientHeight ?? 480;
-    // delete payload.width;
-    // delete payload.height;
-    /* Data Parsing Functions */
-    // mpld3.draw_figure = function(figid, spec, process, clearElem) {}
-    mpld3.draw_figure("profile_div", payload, false, true);
+    if (props.visible) {
+      let plotdata = { ...payload };
+      console.log(plotdata, plot_div.value);
+      plotdata.width = Math.round(plot_div.value?.clientWidth ?? 640) - 32;
+      plotdata.height = Math.round(plot_div.value?.clientHeight ?? 480) - 32;
+      // delete payload.width;
+      // delete payload.height;
+      /* Data Parsing Functions */
+      // mpld3.draw_figure = function(figid, spec, process, clearElem) {}
+      mpld3.draw_figure("profile_div", plotdata, false, true);
+    }
   });
+}
+
+function draw_if_needed(timestamp: number) {
+  if (draw_requested.value) {
+    draw_requested.value = false;
+    fetch_and_draw();
+  }
+  window.requestAnimationFrame(draw_if_needed);
 }
 
 watch(() => props.visible, (value) => {
@@ -48,7 +61,6 @@ defineExpose({
 <template>
   <div class="container d-flex flex-grow-1">
     <div class="flex-grow-1" ref="plot_div" id="profile_div">
-
     </div>
   </div>
 </template>
