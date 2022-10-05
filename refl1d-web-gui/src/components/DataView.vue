aa3cb5b18dc1e376948583d22c834015b34fffcc
Brian Ben Maranville <brian.maranville@nist.gov>
2022-10-05 12:01:25 -0400

    typing updates

--- refl1d-web-gui/src/components/DataView.vue
+++ refl1d-web-gui/src/components/DataView.vue
@@ -26,11 +26,11 @@ function fetch_and_draw() {
     if (plot_div.value === null) {
       return
     }
-    let theory_traces: Plotly.Data[] = [];
-    let data_traces: Plotly.Data[] = [];
+    let theory_traces: (Plotly.Data & {x: number, y: number})[] = [];
+    let data_traces: (Plotly.Data & {x: number, y: number})[] = [];
     for (let model of payload) {
       for (let xs of model) {
-        theory_traces.push({ x: xs.Q, y: xs.theory, mode: 'lines', name: xs.label + ' theory', line: {width: 2}});
+        theory_traces.push({ x: xs.Q, y: xs.theory, mode: 'lines', name: xs.label + ' theory', line: { width: 2 } });
         data_traces.push({ x: xs.Q, y: xs.R, mode: 'markers', name: xs.label + ' data' });
       }
     }
@@ -44,18 +44,18 @@ function fetch_and_draw() {
         autorange: true
       },
       yaxis: {
-        title: {text: 'Reflectivity'},
+        title: { text: 'Reflectivity' },
         exponentformat: 'e',
         showexponent: 'all',
         type: 'log',
         autorange: true
-      }
+      },
     };
 
     if (plot.value !== undefined) {
       const trace_updates = {
         x: [...theory_traces.map((t) => t.x), ...data_traces.map((t) => t.x)],
-        y: [...theory_traces.map((t) => t.y), ...data_traces.map((t) => t.y)],       
+        y: [...theory_traces.map((t) => t.y), ...data_traces.map((t) => t.y)],
       }
       await Plotly.update(plot_div.value, trace_updates, layout);
     }
