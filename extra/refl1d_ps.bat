f0e1b3ece62c9b06c5ce041e9e2b9078f9a4bfc8
bbm <brian.maranville@nist.gov>
2022-10-31 17:04:11 -0400

    define refl1d as a function in PowerShell so it doesn't try to use refl1d.bat (only for CMD.exe?)

--- extra/refl1d_ps.bat
+++ extra/refl1d_ps.bat
@@ -3,4 +3,4 @@
 @REM start PowerShell -noexit -Command "Function refl1d() {&'%~dp0\python.exe' '%~dp0\Scripts\refl1d_cli.py' $args}"
 @REM start PowerShell -noexit -Command "$Env:Path = '%~dp0;%~dp0\Scripts;%Path%'; $Env:PYTHONPATH='"
 set "PATH=%~dp0;%~dp0\Scripts;%PATH%"
-start PowerShell -noexit
+start PowerShell -noexit "Function refl1d() {&'%~dp0\python.exe' '%~dp0\Scripts\refl1d_cli.py' $args}"
