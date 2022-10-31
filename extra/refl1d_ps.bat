d0d1d0cbc5fa36e185fb3a7702a886033f5332b7
bbm <brian.maranville@nist.gov>
2022-10-31 17:07:03 -0400

    consolidate powershell scripts

--- extra/refl1d_ps.bat
+++
@@ -1,6 +0,0 @@
-@REM start powershell.exe -noexit -Command "Function refl1d() {&'C:\Users\bbm\Downloads\Refl1D windows exe latest (NIST)\Refl1D-0.8.14\python.exe' 'C:\Users\bbm\Downloads\Refl1D windows exe latest (NIST)\Refl1D-0.8.14\Scripts\refl1d_cli.py' $args}" 
-@REM start powershell.exe -noexit -Command "Function refl1d() {&'%~dp0\python.exe' '%~dp0\Scripts\refl1d_cli.py' $args}"
-@REM start PowerShell -noexit -Command "Function refl1d() {&'%~dp0\python.exe' '%~dp0\Scripts\refl1d_cli.py' $args}"
-@REM start PowerShell -noexit -Command "$Env:Path = '%~dp0;%~dp0\Scripts;%Path%'; $Env:PYTHONPATH='"
-set "PATH=%~dp0;%~dp0\Scripts;%PATH%"
-start PowerShell -noexit "Function refl1d() {&'%~dp0\python.exe' '%~dp0\Scripts\refl1d_cli.py' $args}"
