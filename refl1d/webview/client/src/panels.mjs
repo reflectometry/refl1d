20f20889efd0f6f162835df624fbf9a107079512
bbm <brian.maranville@nist.gov>
2023-03-20 13:13:30 -0400

    rename data panel to reflectivity

--- refl1d/webview/client/src/panels.mjs
+++ refl1d/webview/client/src/panels.mjs
@@ -5,6 +5,6 @@ import ModelView from './components/ModelView.vue';
 export const panels = [...bumps_panels];
 
 // replace the bumps data view with the refl1d one.
-panels.find(p => p.title === 'Data').component = DataView;
+panels.splice(0, 1, {title: 'Reflectivity', component: DataView});
 // insert the profile panel at position 3
 panels.splice(2, 0, {title: 'Profile', component: ModelView});
