59da1620650f3472601e34d3e9b7f4b49f9cd2f2
bbm <brian.maranville@nist.gov>
2023-05-03 16:28:43 -0400

    pass app name to webview client app

--- refl1d/webview/client/src/main.js
+++ refl1d/webview/client/src/main.js
@@ -4,4 +4,6 @@ import './style.css'
 import App from 'bumps-webview-client/src/App.vue';
 import { panels } from './panels.mjs';
 
-createApp(App, {panels}).mount('#app');
+const name = "Refl1D";
+
+createApp(App, {panels, name}).mount('#app');
