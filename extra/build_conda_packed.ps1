55b5dc278da97497f3b151e90ddf5ef1308aa162
Brian Ben Maranville <brian.maranville@nist.gov>
2023-05-22 09:52:09 -0400

    pull in changed files from master

--- extra/build_conda_packed.ps1
+++ extra/build_conda_packed.ps1
@@ -1,6 +1,6 @@
 $ENV_NAME="isolated-base"
 $PYTHON_VERSION="3.10"
-$DIRNAME="Refl1D-webview"
+$DIRNAME="refl1d"
 
 conda activate "base"
 
@@ -10,21 +10,20 @@ conda-pack -n "$ENV_NAME" -f -o "$ENV_NAME.tar.gz"
 
 # unpack the new environment, that contains only python + pip
 $destdir="$DIRNAME"
-Remove-Item -r "$DIRNAME" -ErrorAction SilentlyContinue
+Remove-Item -r "$DIRNAME"
 mkdir "$destdir"
 tar -xzf "$ENV_NAME.tar.gz" -C "$destdir"
 
 # activate the unpacked environment and install pip packages
 conda deactivate
 $WORKING_DIRECTORY="$pwd"
-& "$destdir\python.exe" -m pip install --no-input --no-compile numba
-& "$destdir\python.exe" -m pip install --no-input --no-compile git+https://github.com/bumps/bumps@webview
-& "$destdir\python.exe" -m pip install --no-input --no-compile git+https://github.com/reflectometry/refl1d@webview
-& "$destdir\python.exe" -m pip install --no-compile -r https://raw.githubusercontent.com/bumps/bumps/webview/webview-requirements
+& "$destdir\python.exe" -m pip install --no-input numba
+& "$destdir\python.exe" -m pip install --no-input git+https://github.com/bumps/bumps@webview
+& "$destdir\python.exe" -m pip install --no-input git+https://github.com/reflectometry/refl1d@webview
+& "$destdir\python.exe" -m pip install -r https://raw.githubusercontent.com/bumps/bumps/webview/webview-requirements
 
 # add our batch script:
-Copy-Item "$PSScriptRoot\refl1d_webview.bat" "$destdir"
+Copy-Item .\refl1d_webview.bat .\refl1d\
 
 # zip it back up
-mkdir "$WORKING_DIRECTORY\dist" -ErrorAction SilentlyContinue
-tar -czf "$WORKING_DIRECTORY\dist\Refl1D-webview-Windows-x86_64.tar.gz" "$DIRNAME"
+tar -czf "$WORKING_DIRECTORY/refl1d-webview-Windows-x86_64.tar.gz" "$DIRNAME"
