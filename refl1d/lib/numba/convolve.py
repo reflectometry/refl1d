4cb219724e89271c5715f635395123cc513c2a3e
Brian Benjamin Maranville <brian.maranville@nist.gov>
2023-06-02 12:42:12 -0400

    refactor numba libraries to be JIT versions of python libraries

--- refl1d/lib/numba/convolve.py
+++ refl1d/lib/numba/convolve.py
@@ -1,117 +1,13 @@
-from math import erf, sqrt, exp
-from . import numba
+import numba
+from .clone_module import clone_module
 
-PI4 = 12.56637061435917295385
-PI_180 = 0.01745329251994329576
-LN256 = 5.54517744447956247533
-SQRT2 = 1.41421356237309504880
-SQRT2PI = 2.50662827463100050241
-LOG_RESLIMIT = -6.90775527898213703123
-root_12_over_2 = sqrt(3)
+MODULE = clone_module('refl1d.lib.python.convolve')
 
+MODULE.prange = numba.prange
 
-@numba.njit('(f8[:], f8[:], f8[:], f8[:], f8[:])', cache=True, parallel=False)
-def convolve_uniform(xi, yi, x, dx, y):
-    left_index = 0
-    N_xi = len(xi)
-    N_x = len(x)
-    for k in numba.prange(N_x):
-        x_k = x[k]
-        # Convert 1-sigma width to 1/2 width of the region
-        limit = dx[k] * root_12_over_2
-        # print(f"point {x_k} +/- {limit}")
-        # Find integration limits, bound by the range of the data
-        left, right = max(x_k - limit, xi[0]), min(x_k + limit, xi[-1])
-        if right < left:
-            # Convolution does not overlap data range.
-            y[k] = 0.
-            continue
+convolve_uniform = numba.njit('(f8[:], f8[:], f8[:], f8[:], f8[:])', cache=True, parallel=False)(MODULE.convolve_uniform)
 
-        # Find the starting point for the convolution by first scanning
-        # forward until we reach the next point greater than the limit
-        # (we might already be there if the next output point has wider
-        # resolution than the current point), then scanning backwards to
-        # get to the last point before the limit. Make sure we have at
-        # least one interval so that we don't have to check edge cases
-        # later.
-        while left_index < N_xi-2 and xi[left_index] < left:
-            left_index += 1
-        while left_index > 0 and xi[left_index] > left:
-            left_index -= 1
-
-        # Set the first interval.
-        total = 0.
-        right_index = left_index + 1
-        x1, y1 = xi[left_index], yi[left_index]
-        x2, y2 = xi[right_index], yi[right_index]
-
-        # Subtract the excess from left interval before the left edge.
-        # print(f" left {left} in {(x1, y1)}, {(x2, y2)}")
-        if x1 < left:
-            # Subtract the area of the rectangle from (x1, 0) to (left, y1)
-            # plus 1/2 the rectangle from (x1, y1) to (left, y'),
-            # where y' is y value where the line (x1, y1) to (x2, y2)
-            # intersects x=left. This can be computed as follows:
-            #    offset = left - x1
-            #    slope = (y2 - y1)/(x2 - x1)
-            #    yleft = y1 + slope*offset
-            #    area = offset * y1 + offset * (yleft-y1)/2
-            # It can be simplified to the following:
-            #    area = offset * (y1 + slope*offset/2)
-            offset = left - x1
-            slope = (y2 - y1)/(x2 - x1)
-            area = offset * (y1 + 0.5*slope*offset)
-            total -= area
-            # print(f" left correction {area}")
-
-        # Do trapezoidal integration up to and including the end interval
-        while right_index < N_xi-1 and x2 < right:
-            # Add the current interval if it isn't empty
-            if x1 != x2:
-                area = 0.5*(y1 + y2)*(x2 - x1)
-                total += area
-                # print(f" adding {(x1,y1)}, {(x2, y2)} as {area}")
-            # Move to the next interval
-            right_index += 1
-            x1, y1, x2, y2 = x2, y2, xi[right_index], yi[right_index]
-        if x1 != x2:
-            area = 0.5*(y1 + y2)*(x2 - x1)
-            total += area
-            # print(f" adding final {(x1,y1)}, {(x2, y2)} as {area}")
-
-        # Subtract the excess from the right interval after the right edge.
-        # print(f" right {right} in {(x1, y1)}, {(x2, y2)}")
-        if x2 > right:
-            # Expression for area to subtract using rectangles is as follows:
-            #    offset = x2 - right
-            #    slope = (y2 - y1)/(x2 - x1)
-            #    yright = y2 - slope*offset
-            #    area = -(offset * yright + offset * (y2-yright)/2)
-            # It can be simplified to the following:
-            #    area = -offset * (y2 - slope*offset/2)
-            offset = x2 - right
-            slope = (y2 - y1)/(x2 - x1)
-            area = offset * (y2 - 0.5*slope*offset)
-            total -= area
-            # print(f" right correction {area}")
-
-        # Normalize by interval length
-        if left < right:
-            # print(f" normalize by length {right} - {left}")
-            y[k] = total / (right - left)
-        elif x1 < x2:
-            # If dx = 0 using the value interpolated at x (with left=right=x).
-            # print(f" dirac delta at {left} = {right} in {(x1, y1)}, {(x2, y2)}")
-            offset = left - x1
-            slope = (y2 - y1)/(x2 - x1)
-            y[k] = y1 + slope*offset
-        else:
-            # At an empty interval in the theory function. Average the y.
-            # print(f" empty interval with {left} = {right} in {(x1, y1)}, {(x2, y2)}")
-            y[k] = 0.5*(y1 + y2)
-
-
-@numba.njit('f8(f8[:], f8[:], i8, i8, f8, f8, f8)', cache=True, parallel=False, locals={
+convolve_gaussian_point = numba.njit('f8(f8[:], f8[:], i8, i8, f8, f8, f8)', cache=True, parallel=False, locals={
     "z": numba.float64,
     "Glo": numba.float64,
     "erflo": numba.float64,
@@ -122,123 +18,16 @@ def convolve_uniform(xi, yi, x, dx, y):
     "erfhi": numba.float64,
     "m": numba.float64,
     "b": numba.float64,
-})
-def convolve_gaussian_point(xin, yin, k, n,
-                            xo, limit, sigma):
-
-    two_sigma_sq = 2. * sigma * sigma
-    # double z, Glo, erflo, erfmin, y
-
-    z = xo - xin[k]
-    Glo = exp(-z*z/two_sigma_sq)
-    erfmin = erflo = erf(-z/(SQRT2*sigma))
-    y = 0.
-    # /* printf("%5.3f: (%5.3f,%11.5g)",xo,xin[k],yin[k]); */
-    while (k < n-1):
-        k += 1
-        if (xin[k] != xin[k-1]):
-            # /* No additional contribution from duplicate points. */
-
-            # /* Compute the next endpoint */
-            zhi = xo - xin[k]
-            Ghi = exp(-zhi*zhi/two_sigma_sq)
-            erfhi = erf(-zhi/(SQRT2*sigma))
-            m = (yin[k]-yin[k-1])/(xin[k]-xin[k-1])
-            b = yin[k] - m * xin[k]
-
-            # /* Add the integrals. */
-            y += 0.5*(m*xo+b)*(erfhi-erflo) - sigma/SQRT2PI*m*(Ghi-Glo)
-
-            # /* Debug computation failures. */
-            # if isnan(y) {
-            #     print("NaN from %d: zhi=%g, Ghi=%g, erfhi=%g, m=%g, b=%g\n",
-            #          % (k,zhi,Ghi,erfhi,m,b))
-            # }
-
-            # /* Save the endpoint for next trapezoid. */
-            Glo = Ghi
-            erflo = erfhi
-
-            # /* Check if we've calculated far enough */
-            if (xin[k] >= xo+limit):
-                break
-
-    # /* printf(" (%5.3f,%11.5g)",xin[k<n?k:n-1],yin[k<n?k:n-1]); */
-
-    # /* Normalize by the area of the truncated gaussian */
-    # /* At this point erflo = erfmax */
-    # /* printf ("---> %11.5g\n",2*y/(erflo-erfmin)); */
-    return 2 * y / (erflo - erfmin)
-
+})(MODULE.convolve_gaussian_point)
+MODULE.convolve_gaussian_point = convolve_gaussian_point
 
 # has same performance when using guvectorize instead of njit:
 # @numba.guvectorize("(i8, f8[:], f8[:], i8, f8[:], f8[:], f8[:])", '(),(m),(m),(),(n),(n)->(n)')
 
-@numba.njit("(f8[:], f8[:], f8[:], f8[:], f8[:])", cache=True, parallel=False, locals={
+convolve_gaussian = numba.njit("(f8[:], f8[:], f8[:], f8[:], f8[:])", cache=True, parallel=False, locals={
     "sigma": numba.float64,
     "xo": numba.float64,
     "limit": numba.float64,
     "k_in": numba.int64,
     "k_out": numba.int64,
-})
-def convolve_gaussian(xin, yin, x, dx, y):
-    # size_t in,out;
-    Nin = len(xin)
-    Nout = len(x)
-
-    # /* FIXME fails if xin are not sorted; slow if x not sorted */
-    # assert(Nin>1)
-
-    # /* Scan through all x values to be calculated */
-    # /* Re: omp, each thread is going through the entire input array,
-    # * independently, computing the resolution from the neighbourhood
-    # * around its individual output points.  The firstprivate(in)
-    # * clause sets each thread to keep its own copy of in, initialized
-    # * at in's initial value of zero.  The "schedule(static,1)" clause
-    # * puts neighbouring points in separate threads, which is a benefit
-    # * since there will be less backtracking if resolution width increases
-    # * from point to point.  Because the schedule is static, this does not
-    # * significantly increase the parallelization overhead.  Because the
-    # * threads are operating on interleaved points, there should be fewer cache
-    # * misses than if each thread were given different stretches of x to
-    # * convolve.
-    # */
-    k_in = 0
-
-    for k_out in range(Nout):
-        # /* width of resolution window for x is w = 2 dx^2. */
-        sigma = dx[k_out]
-        xo = x[k_out]
-        limit = sqrt(-2.*sigma*sigma * LOG_RESLIMIT)
-
-        # // if (out%20==0)
-
-        # /* Line up the left edge of the convolution window */
-        # /* It is probably forward from the current position, */
-        # /* but if the next dx is a lot higher than the current */
-        # /* dx or if the x are not sorted, then it may be before */
-        # /* the current position. */
-        # /* FIXME verify that the convolution window is just right */
-        while (k_in < Nin-1 and xin[k_in] < xo-limit):
-            k_in += 1
-        while (k_in > 0 and xin[k_in] > xo-limit):
-            k_in -= 1
-
-        # /* Special handling to avoid 0/0 for w=0. */
-        if (sigma > 0.):
-            y[k_out] = convolve_gaussian_point(
-                xin, yin, k_in, Nin, xo, limit, sigma)
-        elif (k_in < Nin-1):
-            # /* Linear interpolation */
-            m = (yin[k_in+1]-yin[k_in])/(xin[k_in+1]-xin[k_in])
-            b = yin[k_in] - m*xin[k_in]
-            y[k_out] = m*xo + b
-        elif (k_in > 0):
-            # /* Linear extrapolation */
-            m = (yin[k_in]-yin[k_in-1])/(xin[k_in]-xin[k_in-1])
-            b = yin[k_in] - m*xin[k_in]
-            y[k_out] = m*xo + b
-        else:
-            # /* Can't happen because there is more than one point in xin. */
-            # assert(Nin>1)
-            pass
+})(MODULE.convolve_gaussian)
--- refl1d/lib/numba/rebin.py
+++ refl1d/lib/numba/rebin.py
@@ -1,5 +1,7 @@
-import math
-from . import numba
+import numba
+from .clone_module import clone_module
+
+MODULE = clone_module('refl1d.lib.python.rebin')
 
 # Define a bin iterator to adapt to either forward or reversed inputs.
 spec = [
@@ -12,167 +14,15 @@ spec = [
     ('atend', numba.boolean)
 ]
 
+BinIter = numba.experimental.jitclass(spec)(MODULE.BinIter)
+MODULE.BinIter = BinIter
 
-@numba.experimental.jitclass(spec)
-class BinIter:
-    def __init__(self, n, edges):
-        self.n = n
-        self.edges = edges
-        self.forward = edges[0] < edges[n]
+rebin_counts_portion = numba.njit(parallel=False, cache=True)(MODULE.rebin_counts_portion)
+MODULE.rebin_counts_portion = rebin_counts_portion
 
-        if (self.forward):
-            self.bin = 0
-            self.lo = edges[0]
-            self.hi = edges[1]
-        else:
-            self.bin = n - 1
-            self.lo = edges[n]
-            self.hi = edges[n-1]
+rebin_counts = numba.njit(parallel=False, cache=True)(MODULE.rebin_counts)
+MODULE.rebin_counts = rebin_counts
 
-        self.atend = n < 1
+rebin_intensity = numba.njit(parallel=False, cache=True)(MODULE.rebin_intensity)
 
-    def increment(self):
-        if self.atend:
-            raise IndexError("moving beyond final bin")
-        self.lo = self.hi
-        if self.forward:
-            self.bin += 1
-            self.atend = (self.bin >= self.n)
-            if (not self.atend):
-                self.hi = self.edges[self.bin+1]
-        else:
-            self.atend = (self.bin == 0)
-            if not self.atend:
-                self.bin -= 1
-                self.hi = self.edges[self.bin]
-        return self
-
-
-@numba.njit(parallel=False, cache=True)
-def rebin_counts_portion(Nold, vold, Iold, Nnew, vnew, Inew, ND_portion):
-
-    # Note: inspired by rebin from OpenGenie, but using counts per bin
-    # rather than rates.
-
-    # Does not work in place
-    if (Iold is Inew):
-        raise ValueError("does not work in place")
-
-    # Traverse both sets of bin edges; if there is an overlap, add the portion
-    # of the overlapping old bin to the new bin.
-    _from = BinIter(Nold, vold)
-    _to = BinIter(Nnew, vnew)
-    while (not _from.atend and not _to.atend):
-        # std::cout << "from " << from.bin << ": [" << from.lo << ", " << from.hi << "]\n";
-        # std::cout << "to " << to.bin << ": [" << to.lo << ", " << to.hi << "]\n";
-        if (_to.hi <= _from.lo):
-            _to.increment()  # new must catch up to old
-        elif (_from.hi <= _to.lo):
-            _from.increment()  # old must catch up to new
-        else:
-            overlap = min(_from.hi, _to.hi) - max(_from.lo, _to.lo)
-            portion = overlap/(_from.hi - _from.lo)
-            Inew[_to.bin] += Iold[_from.bin]*portion*ND_portion
-            if (_to.hi > _from.hi):
-                _from.increment()
-            else:
-                _to.increment()
-
-
-@numba.njit(parallel=False, cache=True)
-def rebin_counts_old(Nold, xold, Iold, Nnew, xnew, Inew):
-
-    # Note: inspired by rebin from OpenGenie, but using counts per bin
-    # rather than rates.
-
-    # Clear the new bins
-    for i in range(Nnew):
-        Inew[i] = 0
-
-    rebin_counts_portion(Nold, xold, Iold, Nnew, xnew, Inew, 1.)
-
-
-@numba.njit(parallel=False, cache=True)
-def rebin_counts(xold, Iold, xnew, Inew):
-
-    # Note: inspired by rebin from OpenGenie, but using counts per bin
-    # rather than rates.
-    Nold = len(Iold)
-    Nnew = len(Inew)
-    # Clear the new bins
-    for i in range(Nnew):
-        Inew[i] = 0
-
-    rebin_counts_portion(Nold, xold, Iold, Nnew, xnew, Inew, 1.)
-
-
-@numba.njit(parallel=False, cache=True)
-def rebin_intensity(Nold, xold, Iold, dIold, Nnew, xnew, Inew, dInew):
-
-    # Note: inspired by rebin from OpenGenie, but using counts per bin rather than rates.
-
-    # Clear the new bins
-    for i in range(Nnew):
-        dInew[i] = Inew[i] = 0
-
-    # Traverse both sets of bin edges; if there is an overlap, add the portion
-    # of the overlapping old bin to the new bin.
-    _from = BinIter(Nold, xold)
-    _to = BinIter(Nnew, xnew)
-    while (not _from.atend and not _to.atend):
-        # std::cout << "from " << from.bin << ": [" << from.lo << ", " << from.hi << "]\n";
-        # std::cout << "to " << to.bin << ": [" << to.lo << ", " << to.hi << "]\n";
-        if (_to.hi <= _from.lo):
-            _to.increment()  # new must catch up to old
-        elif (_from.hi <= _to.lo):
-            _from.increment()  # old must catch up to new
-        else:
-            overlap = min(_from.hi, _to.hi) - max(_from.lo, _to.lo)
-            portion = overlap/(_from.hi - _from.lo)
-
-            Inew[_to.bin] += (Iold[_from.bin])*portion
-            # add in quadrature
-            dInew[_to.bin] += (dIold[_from.bin]*portion)**2
-            if (_to.hi > _from.hi):
-                _from.increment()
-            else:
-                _to.increment()
-
-    # Convert variance to standard deviation.
-    for i in range(Nnew):
-        dInew[i] = math.sqrt(dInew[i])
-
-
-@numba.njit(cache=True)
-def rebin_counts_2D(xold, yold, Iold, xnew, ynew, Inew):
-    Nxold = len(xold) - 1
-    Nyold = len(yold) - 1
-    Nxnew = len(xnew) - 1
-    Nynew = len(ynew) - 1
-
-    # Clear the new bins
-    for i in range(Nxnew):
-        for j in range(Nynew):
-            Inew[i][j] = 0
-
-    # Traverse both sets of bin edges; if there is an overlap, add the portion
-    # of the overlapping old bin to the new bin.  Scale this by the portion
-    # of the overlap in y.
-    _from = BinIter(Nxold, xold)
-    _to = BinIter(Nxnew, xnew)
-
-    while (not _from.atend and not _to.atend):
-        if (_to.hi <= _from.lo):
-            _to.increment()  # new must catch up to old
-        elif (_from.hi <= _to.lo):
-            _from.increment()  # old must catch up to new
-        else:
-            overlap = min(_from.hi, _to.hi) - max(_from.lo, _to.lo)
-            portion = overlap / (_from.hi - _from.lo)
-            rebin_counts_portion(Nyold, yold, Iold[_from.bin],
-                                 Nynew, ynew, Inew[_to.bin],
-                                 portion)
-            if (_to.hi > _from.hi):
-                _from.increment()
-            else:
-                _to.increment()
+rebin_counts_2D = numba.njit(cache=True)(MODULE.rebin_counts_2D)
