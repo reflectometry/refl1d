01c11a7c0eb5fc578bff077a349af5b304703695
Brian Benjamin Maranville <brian.maranville@nist.gov>
2023-06-26 13:23:31 -0400

    Update build_conda_packed.ps1

--- extra/build_conda_packed.ps1
+++ extra/build_conda_packed.ps1
@@ -9,7 +9,7 @@ conda create -n "$ENV_NAME" -q --force -y "python=$PYTHON_VERSION"
 conda-pack -n "$ENV_NAME" -f -o "$ENV_NAME.tar.gz"
 
 # unpack the new environment, that contains only python + pip
-$destdir="$DIRNAME"
+$destdir="dist\$DIRNAME"
 Remove-Item -r "$DIRNAME"
 mkdir "$destdir"
 tar -xzf "$ENV_NAME.tar.gz" -C "$destdir"
@@ -29,4 +29,4 @@ Copy-Item .\extra\refl1d_webview.bat "$destdir"
 & "$destdir\python.exe" -m pip install -r https://raw.githubusercontent.com/bumps/bumps/webview/webview-requirements
 
 # zip it back up
-tar -czf "$WORKING_DIRECTORY/refl1d-webview-Windows-x86_64.tar.gz" "$DIRNAME"
+tar -czf "$WORKING_DIRECTORY/refl1d-webview-Windows-x86_64.tar.gz" "$destdir"
