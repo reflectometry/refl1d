a4a434b8e0358e8f5481477484d26bd1c9c2f87f
Brian Benjamin Maranville <brian.maranville@nist.gov>
2023-10-27 16:32:42 -0400

    update conda builder for mac/linux

--- extra/build_conda_packed.sh
+++ extra/build_conda_packed.sh
@@ -8,7 +8,7 @@ eval "$(conda shell.bash hook)"
 conda activate base || { echo 'failed: conda not installed'; exit 1; }
 
 conda install -y conda-pack
-conda create -n "$ENV_NAME" -y "python=$PYTHON_VERSION"
+conda create -n "$ENV_NAME" -q --force -y "python=$PYTHON_VERSION"
 conda-pack -n "$ENV_NAME" -f -o "$ENV_NAME.tar.gz"
 
 # unpack the new environment, that contains only python + pip
@@ -20,11 +20,13 @@ tar -xzf "$ENV_NAME.tar.gz" -C "$destdir"
 # activate the unpacked environment and install pip packages
 conda deactivate
 WORKING_DIRECTORY=$(pwd)
-cd "$tmpdir"
+# cd "$tmpdir"
 $destdir/bin/python -m pip install --no-input numba
 $destdir/bin/python -m pip install --no-input git+https://github.com/bumps/bumps@webview
 $destdir/bin/python -m pip install --no-input git+https://github.com/reflectometry/refl1d@webview
-$destdir/bin/python -m pip install -r https://raw.githubusercontent.com/reflectometry/refl1d/webview/webview-requirements
+$destdir/bin/python -m pip install -r https://raw.githubusercontent.com/bumps/bumps/webview/webview-requirements
 
-# zip it back up
-tar -czf "$WORKING_DIRECTORY/refl1d-webview-$(uname -s)-$(uname -m).tar.gz" "$DIRNAME"
+version=$($destdir/bin/python -c "import refl1d; print(refl1d.__version__)")
+mv "$tmpdir/$DIRNAME" "$tmpdir/$DIRNAME-$version"
+
+cd $tmpdir && tar -czf "$WORKING_DIRECTORY/refl1d-webview-$version-$(uname -s)-$(uname -m).tar.gz" "$DIRNAME-$version"
