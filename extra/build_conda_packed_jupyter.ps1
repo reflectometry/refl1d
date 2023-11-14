4a2a86acb1d91f8bf4abaf8c61c7da24c3f42d31
Brian Ben Maranville <brian.maranville@nist.gov>
2023-11-14 12:26:01 -0500

    copy new refl1d_jupyter.bat to package

--- extra/build_conda_packed_jupyter.ps1
+++ extra/build_conda_packed_jupyter.ps1
@@ -24,6 +24,7 @@ dir .
 dir ..
 # add our batch script:
 Copy-Item .\extra\refl1d_webview.bat "$destdir"
+Copy-Item .\extra\refl1d_jupyter.bat "$destdir"
 
 & "$destdir\python.exe" -m pip install --no-input --no-compile numba jupyterlab notebook
 & "$destdir\python.exe" -m pip install --no-input --no-compile git+https://github.com/bumps/bumps@webview
