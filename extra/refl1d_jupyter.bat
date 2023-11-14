36de04294b92cc3187f68fb724c61917bd99a23d
Brian Ben Maranville <brian.maranville@nist.gov>
2023-11-14 12:21:57 -0500

    adding build recipe that includes jupyter lab server

---
+++ extra/refl1d_jupyter.bat
@@ -0,0 +1,4 @@
+@echo off
+
+rem start "Refl1d Jupyter Session"
+start "Refl1D Jupyter Server" "%~dp0\python.exe" "-c" "from tkinter import filedialog as fd; import jupyterlab.labapp as lab; start_folder = fd.askdirectory(); lab.main(argv=[f'--notebook-dir={start_folder}'])"
