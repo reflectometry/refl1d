5e5ed8b01e2b66ae9ab470adc27b7c3ba54bcc22
bbm <brian.maranville@nist.gov>
2023-12-12 16:27:49 -0500

    import types from typing directly

--- refl1d/magnetism.py
+++ refl1d/magnetism.py
@@ -40,11 +40,12 @@ and anchoring them to the structure.
 from __future__ import print_function
 
 from dataclasses import dataclass, field
+from typing import Optional, Any, Union, Dict, Callable, Literal, Tuple, List, Literal
+
 import numpy as np
 from numpy import inf
 from bumps.parameter import Parameter, flatten, to_dict
 from bumps.mono import monospline
-from bumps.util import Optional, Any, Union, Dict, Callable, Literal, Tuple, List, Literal
 
 from .reflectivity import BASE_GUIDE_ANGLE as DEFAULT_THETA_M
 
