0001b680262236393ec3efccedd4bb7f48d1422a
bbm <brian.maranville@nist.gov>
2024-06-06 10:56:30 -0400

    handle replacement of bumps panels (and insertions of new panels) by name instead of position

--- refl1d/webview/client/src/panels.mjs
+++ refl1d/webview/client/src/panels.mjs
@@ -4,11 +4,47 @@ import ModelView from './components/ModelView.vue';
 import SimpleBuilder from './components/SimpleBuilder.vue';
 import ProfileUncertaintyView from './components/ProfileUncertaintyView.vue';
 
-export const panels = [...bumps_panels];
+const refl1d_panels = [
+    {title: 'Reflectivity', component: DataView},
+    {title: 'Profile', component: ModelView},
+    {title: 'Profile Uncertainty', component: ProfileUncertaintyView},
+    {title: 'Builder', component: SimpleBuilder},
+];
 
-// replace the bumps data view with the refl1d one.
-panels.splice(0, 1, {title: 'Reflectivity', component: DataView});
-// insert the profile panel at position 3
-panels.splice(2, 0, {title: 'Profile', component: ModelView});
-panels.splice(9, 1, {title: 'Profile Uncertainty', component: ProfileUncertaintyView});
-panels.push({title: 'Builder', component: SimpleBuilder})
+const replacements = {
+    'Reflectivity': 'Data',
+    'Profile Uncertainty': 'Model Uncertainty',
+}
+
+const insertions = {
+    'Profile': { after: 'Summary'},
+    'Builder': { after: 'Uncertainty' }
+}
+
+function replace_panel(panels, replacement_panels, replaced_title, replacement_title) {
+    const index = panels.findIndex(p => p.title === replaced_title);
+    const replacement_index = replacement_panels.findIndex(p => p.title === replacement_title);
+    if (index >= 0 && replacement_index >= 0) {
+        panels.splice(index, 1, replacement_panels[replacement_index]);
+    }
+}
+
+function insert_panel(panels, insertion_panels, title, after) {
+    let index = panels.findIndex(p => p.title === after);
+    // put it at the end if the after panel is not found
+    if (index < 0) {
+        index = panels.length - 1;
+    }
+    const insertion_index = insertion_panels.findIndex(p => p.title === title);
+    if (insertion_index >= 0) {
+        panels.splice(index+1, 0, insertion_panels[insertion_index]);
+    }
+}
+
+export const panels = [...bumps_panels];
+for (const [replacement_title, replaced_title] of Object.entries(replacements)) {
+    replace_panel(panels, refl1d_panels, replaced_title, replacement_title);
+}
+for (const [title, {after}] of Object.entries(insertions)) {
+    insert_panel(panels, refl1d_panels, title, after);
+}
