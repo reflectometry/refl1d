fe8542ffa5fee5ceb10c31f5028a578c2072c247
bbm <brian.maranville@nist.gov>
2024-07-01 14:41:32 -0400

    link to the locally-built bumps client instead of using npm installed version, by default

--- refl1d/webview/build_client.py
+++ refl1d/webview/build_client.py
@@ -1,7 +1,7 @@
 from typing import Literal
 
-def build_client(install_dependencies=True, mode: Literal["development", "production"]="development", sourcemap=False):
-    """Build the webview client."""
+def build_client(install_dependencies=False, mode: Literal["development", "production"]="development", sourcemap=False, link_bumps=True):
+    """Build the refl1d webview client."""
     from pathlib import Path
     import os
     import shutil
@@ -15,6 +15,12 @@ def build_client(install_dependencies=True, mode: Literal["development", "produc
     if install_dependencies or not node_modules.exists():
         print("Installing node modules...")
         os.system("npm install")
+    # link to the local version of bumps:
+    if link_bumps:
+        import bumps.webview
+        bumps_path = Path(bumps.webview.__file__).parent / 'client'
+        print(f"Linking to the local version of bumps... {bumps_path}")
+        os.system(f"npm link {bumps_path}")
     # build the client
     print("Building the webview client...")
     cmd = f"npm run build -m {mode}"
@@ -29,5 +35,6 @@ if __name__ == "__main__":
     parser.add_argument("--install-dependencies", action="store_true", help="Install dependencies.")
     parser.add_argument("--mode", choices=["development", "production"], default="development", help="Build mode.")
     parser.add_argument("--sourcemap", action="store_true", help="Generate sourcemaps.")
+    parser.add_argument("--link-bumps", action="store_false", help="Link to the local version of bumps.")
     args = parser.parse_args()
-    build_client(install_dependencies=args.install_dependencies, mode=args.mode, sourcemap=args.sourcemap)
+    build_client(install_dependencies=args.install_dependencies, mode=args.mode, sourcemap=args.sourcemap, link_bumps=args.link_bumps)
