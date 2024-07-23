41028c9ef10c26e0941d90042f8c2271c3a01c62
Brian Ben Maranville <brian.maranville@nist.gov>
2024-07-23 10:41:50 -0400

    build self-extracting exe in powershell directly

--- extra/build_conda_packed.ps1
+++ extra/build_conda_packed.ps1
@@ -1,10 +1,11 @@
 $ENV_NAME="isolated-base"
 $PYTHON_VERSION="3.10"
-$DIRNAME="refl1d"
+$PKGNAME="refl1d"
+$SUBNAME="webview"
 
 conda activate "base"
 
-conda install -y conda-pack
+conda install -y conda-pack 7zip
 conda create -n "$ENV_NAME" -q --force -y "python=$PYTHON_VERSION" "nodejs"
 conda-pack -n "$ENV_NAME" -f -o "$ENV_NAME.tar.gz"
 
@@ -12,7 +13,7 @@ conda-pack -n "$ENV_NAME" -f -o "$ENV_NAME.tar.gz"
 $tmp_target_dir="dist"
 mkdir -Force $tmp_target_dir
 $tmpdir = Resolve-Path "$tmp_target_dir" | Select-Object -ExpandProperty Path
-$destdir="$tmpdir\$DIRNAME"
+$destdir="$tmpdir\$PKGNAME"
 $envdir = "$destdir\env"
 Remove-Item -r -Force "$destdir"
 mkdir "$envdir"
@@ -22,8 +23,6 @@ tar -xzf "$ENV_NAME.tar.gz" -C "$envdir"
 conda deactivate
 $WORKING_DIRECTORY="$pwd"
 echo "WORKING_DIRECTORY=$WORKING_DIRECTORY"
-dir .
-dir ..
 # add our batch script:
 Copy-Item .\extra\platform_scripts\refl1d_webview.bat "$destdir"
 
@@ -35,7 +34,6 @@ Copy-Item .\extra\platform_scripts\refl1d_webview.bat "$destdir"
 # build the client
 cd $envdir\Lib\site-packages\bumps\webview\client
 & "$envdir\npm.cmd" install
-& "$envdir\npm.cmd" link
 
 cd $envdir\Lib\site-packages\refl1d\webview\client
 & "$envdir\npm.cmd" link ..\..\..\bumps\webview\client
@@ -43,12 +41,18 @@ cd $envdir\Lib\site-packages\refl1d\webview\client
 & "$envdir\npm.cmd" run build
 
 # remove node_modules after build
-rm -r -Force $envdir\Lib\site-packages\bumps\webview\client\node_modules
 rm -r -Force $envdir\Lib\site-packages\refl1d\webview\client\node_modules
+rm -r -Force $envdir\Lib\site-packages\bumps\webview\client\node_modules
+rm -r -Force $envdir\node_modules
 
-$version=$(& "$envdir\python.exe" -c "import refl1d; print(refl1d.__version__)")
+$version=$(& "$envdir\python.exe" -c "import $PKGNAME; print($PKGNAME.__version__)")
 # zip it back up
 cd $tmpdir
-Rename-Item "$DIRNAME" "$DIRNAME-$version"
-tar -czf "refl1d-webview-Windows-x86_64.tar.gz" "$DIRNAME-$version"
-Compress-Archive -Path "$DIRNAME-$version" -DestinationPath "refl1d-webview-Windows-x86_64.zip"
+Rename-Item "$PKGNAME" "$PKGNAME-$version"
+tar -czf "$PKGNAME-$SUBNAME-$version-Windows-x86_64.tar.gz" "$PKGNAME-$version"
+Compress-Archive -Path "$PKGNAME-$version" -DestinationPath "$PKGNAME-$SUBNAME-$version-Windows-x86_64.zip"
+
+# build self-extracting executable
+Invoke-WebRequest https://www.7-zip.org/a/7z2106-x64.exe -OutFile 7z.exe
+7z e 7z.exe -aoa 7z.sfx
+7z a -mhe=on -mx=1 -sfx".\7z.sfx" "$PKGNAME-$SUBNAME-$version-Windows-x86_64-self-extracting.exe" "$PKGNAME-$version"
