25a2ac403093a9acd7729c79f146a120486c79cb
bbm <brian.maranville@nist.gov>
2024-08-06 17:14:41 -0400

    wrap lib/python/build_profile for numba, as is done for the other lib routines

--- refl1d/lib/numba/build_profile.py
+++ refl1d/lib/numba/build_profile.py
@@ -2,6 +2,10 @@ import numba
 import numpy as np
 from math import erf
 
+from .clone_module import clone_module
+
+MODULE = clone_module('refl1d.lib.python.build_profile')
+
 SQRT1_2 = 1. / np.sqrt(2.0)
 
 @numba.vectorize
@@ -9,54 +13,6 @@ def verf(x):
     return erf(x)
 
 @numba.njit(cache=True)
-def _build_profile(z, offset, roughness, contrast, initial_value, profiles):
-    """
-    Convert a step profile to a smooth profile.
-
-    *z*          calculation points, shape = (NZ,)
-    *offset*     offset for each interface, shape = (NI,)
-    *roughness*  roughness of each interface, shape = (NI,)
-    *contrast*   step value at each interface for each profile, shape = (NP * NI)
-    *initial_value* starting value for each profile, shape = (NP)
-
-    *profiles*   (output) results of calculation
-    """
-    # import time
-    # start_time = time.time()
-    # contrast has shape (NP, NI)
-    # Number of z values:
-    NZ = len(z)
-    # Number of wavelength values:
-    NP = initial_value.shape[0]
-    # Number of interfaces:
-    NI = len(offset)
-
-    contrast_shaped = contrast.reshape((NP, NI))
-    profiles_shaped = profiles.reshape((NP, NZ)) # view - updates affect profiles
-
-    profiles_shaped += initial_value.reshape((NP, 1))
-    for i in range(NI):
-        offset_i = offset[i]
-        sigma_i = roughness[i]
-        contrast_i = contrast_shaped[:, i]
-        blended = 1.0 * (z >= offset_i) if sigma_i <= 0.0 else 0.5 * verf(SQRT1_2 * (z - offset_i) / sigma_i) + 0.5
-        # contrast_i has shape (NP,) and blended has shape (NZ,)
-        delta = contrast_i.copy().reshape((NP, 1)) * blended.reshape((1, NZ))
-        # delta has shape (NL, NZ) like result
-        profiles_shaped += delta
-
-    # end_time = time.time()
-    # print(f"blend: {end_time - start_time}")
-    return
-
-def build_profile(z, offset, roughness, contrast, initial_value, profiles):
-    # import time
-    # start_time = time.time()
-    _build_profile(z, offset, roughness, contrast, initial_value, profiles)
-    # end_time = time.time()
-    # print(f"build_profile: {end_time - start_time}")
-    return
-
 def blend(z, sigma, offset):
     """
     blend function
@@ -67,4 +23,11 @@ def blend(z, sigma, offset):
     if sigma <= 0.0:
         return 1.0 * (z >= offset)
     else:
-        return 0.5 * erf(SQRT1_2 * (z - offset) / sigma) + 0.5
+        return 0.5 * verf(SQRT1_2 * (z - offset) / sigma) + 0.5
+
+MODULE.blend = blend
+
+# BUILD_PROFILE_SIG = 'void(f8[:], f8[:], f8[:], f8[:], f8[:], f8[:,:])'
+# build_profile = numba.njit(BUILD_PROFILE_SIG)(MODULE.build_profile)
+build_profile = numba.njit(cache=True)(MODULE.build_profile)
+MODULE.build_profile = build_profile
