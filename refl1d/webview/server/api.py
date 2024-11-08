319e7377cee79680591d4fe15127cee88bcec3d1
Brian Benjamin Maranville <brian.maranville@nist.gov>
2024-11-08 11:04:21 -0500

    calculate the profile uncertainty in a thread

--- refl1d/webview/server/api.py
+++ refl1d/webview/server/api.py
@@ -1,6 +1,10 @@
+import asyncio
+from copy import deepcopy
+from functools import lru_cache
 from typing import Union, Dict, List, TypedDict
 from pathlib import Path
 import numpy as np
+from refl1d.errors import calc_errors
 from refl1d.experiment import Experiment, ExperimentBase, MixedExperiment
 import refl1d.probe
 from bumps.webview.server.api import (
@@ -16,7 +20,7 @@ from bumps.webview.server.api import (
     logger,
 )
 import bumps.webview.server.api as bumps_api
-from bumps.errplot import calc_errors_from_state
+from bumps.errplot import error_points_from_state
 
 # from refl1d.errors import show_errors
 from .profile_uncertainty import show_errors
@@ -126,8 +130,8 @@ async def get_model_names():
     return output
 
 
-@register
-async def get_profile_uncertainty_plot(
+@lru_cache(maxsize=30)
+def _get_profile_uncertainty_plot(
     auto_align: bool = True,
     align: float = 0.0,
     nshown: int = 5000,
@@ -137,7 +141,7 @@ async def get_profile_uncertainty_plot(
 ):
     if state.problem is None or state.problem.fitProblem is None:
         return None
-    fitProblem = state.problem.fitProblem
+    fitProblem = deepcopy(state.problem.fitProblem)
     uncertainty_state = state.fitting.uncertainty_state
     align_arg = "auto" if auto_align else align
     if uncertainty_state is not None:
@@ -145,7 +149,9 @@ async def get_profile_uncertainty_plot(
 
         start_time = time.time()
         logger.info(f"queueing new profile uncertainty plot... {start_time}")
-        errs = calc_errors_from_state(fitProblem, uncertainty_state, nshown=nshown, random=random, portion=1.0)
+        error_points = error_points_from_state(uncertainty_state, nshown=nshown, random=random, portion=1.0)
+        logger.info(f"points calculated: {time.time() - start_time}")
+        errs = calc_errors(fitProblem, error_points)
         logger.info(f"errors calculated: {time.time() - start_time}")
         error_result = show_errors(errs, npoints=npoints, align=align_arg, residuals=residuals)
         error_result["fig"] = error_result["fig"].to_dict()
@@ -157,6 +163,27 @@ async def get_profile_uncertainty_plot(
         return output
     else:
         return None
+
+
+@register
+async def get_profile_uncertainty_plot(
+    auto_align: bool = True,
+    align: float = 0.0,
+    nshown: int = 5000,
+    npoints: int = 5000,
+    random: bool = True,
+    residuals: bool = False,
+):
+    result = await asyncio.to_thread(
+        _get_profile_uncertainty_plot,
+        auto_align=auto_align,
+        align=align,
+        nshown=nshown,
+        npoints=npoints,
+        random=random,
+        residuals=residuals,
+    )
+    return result
 
 
 @register
