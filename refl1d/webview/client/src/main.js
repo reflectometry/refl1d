76b7abb924e4088f264f86a660556c3503e107dc
glass-ships <glass.ships@outlook.com>
2024-11-15 11:34:09 -0500

    initial commit

--- refl1d/webview/client/src/main.js
+++ refl1d/webview/client/src/main.js
@@ -1,31 +1,31 @@
-import { createApp, computed } from 'vue'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.css'
-import { model_file, menu_items, FileBrowserSettings, fileBrowser, socket } from 'bumps-webview-client/src/app_state';
-import App from 'bumps-webview-client/src/App.vue';
-import { panels } from './panels.mjs';
-import { dq_is_FWHM } from './app_state';
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "bumps-webview-client/src/App.vue";
+import { fileBrowser, menu_items, model_file, socket } from "bumps-webview-client/src/app_state";
+import { computed, createApp } from "vue";
+import { dqIsFWHM } from "./app_state";
+import { panels } from "./panels.mjs";
+import "./style.css";
 
 const name = "Refl1D";
 
-async function loadProbeFromFile(ev) {
+async function loadProbeFromFile() {
   if (fileBrowser.value) {
     const settings = {
       title: "Load Probe Data from File",
       callback: (pathlist, filename) => {
-        socket.value.asyncEmit("load_probe_from_file", pathlist, filename, 0, dq_is_FWHM.value);
+        socket.value.asyncEmit("load_probe_from_file", pathlist, filename, 0, dqIsFWHM.value);
       },
       show_name_input: true,
       name_input_label: "Filename",
       require_name: true,
       show_files: true,
       chosenfile_in: "",
-      search_patterns: [".ort", ".orb", ".refl", ".dat", ".txt"]
+      search_patterns: [".ort", ".orb", ".refl", ".dat", ".txt"],
     };
     fileBrowser.value.open(settings);
   }
 }
 
-createApp(App, {panels, name}).mount('#app');
-const model_not_loaded = computed(() => model_file.value == null);
-menu_items.value.push({ text: "Load Data into Model", action: loadProbeFromFile, disabled: model_not_loaded });
+createApp(App, { panels, name }).mount("#app");
+const modelNotLoaded = computed(() => model_file.value == null);
+menu_items.value.push({ text: "Load Data into Model", action: loadProbeFromFile, disabled: modelNotLoaded });
--- refl1d/webview/client/src/model.ts
+++ refl1d/webview/client/src/model.ts
@@ -1,99 +1,104 @@
+/**
+ * This file contains the types for the serialized model format used by the refl1d webapp.
+ * ! Note that the fields must match the Python model serialization format, and must therefore be snake_case
+ */
+
 export interface SerializedModel {
-    references: { [key: string]: Parameter };
-    object: any;
-    "$schema": "bumps-draft-02";
+  references: { [key: string]: Parameter };
+  object: any;
+  $schema: "bumps-draft-02";
 }
 
 export interface Variable {
-    value: number;
-    __class__: "bumps.parameter.Variable";
+  value: number;
+  __class__: "bumps.parameter.Variable";
 }
 
 export interface Reference {
-    id: string;
-    __class__: "Reference";
+  id: string;
+  __class__: "Reference";
 }
 
 export type BoundsValue = number | "-inf" | "inf";
 
 export interface Parameter {
-    id: string;
-    name: string;
-    fixed: boolean;
-    slot: Variable;
-    limits: (number | "-inf" | "inf")[];
-    bounds: (number | "-inf" | "inf")[] | null;
-    tags: string[];
-    __class__: "bumps.parameter.Parameter";
+  id: string;
+  name: string;
+  fixed: boolean;
+  slot: Variable;
+  limits: (number | "-inf" | "inf")[];
+  bounds: (number | "-inf" | "inf")[] | null;
+  tags: string[];
+  __class__: "bumps.parameter.Parameter";
 }
 
 export interface ParameterLike {
-    id: string;
-    __class__: "bumps.parameter.Parameter" | "Reference";
+  id: string;
+  __class__: "bumps.parameter.Parameter" | "Reference";
 }
 
 export interface Slab {
-    name: string;
-    thickness: ParameterLike;
-    magnetism: Magnetism | null;
-    material: SLD;
-    interface: ParameterLike;
-    __class__: "refl1d.model.Slab";
+  name: string;
+  thickness: ParameterLike;
+  magnetism: Magnetism | null;
+  material: SLD;
+  interface: ParameterLike;
+  __class__: "refl1d.model.Slab";
 }
 
 export interface Repeat {
-    name: string;
-    interface: ParameterLike;
-    magnetism: Magnetism | null;
-    repeat: ParameterLike;
-    stack: Stack;
-    thickness: ParameterLike;
-    __class__: "refl1d.model.Repeat";
+  name: string;
+  interface: ParameterLike;
+  magnetism: Magnetism | null;
+  repeat: ParameterLike;
+  stack: Stack;
+  thickness: ParameterLike;
+  __class__: "refl1d.model.Repeat";
 }
 
 export interface SLD {
-    name: string;
-    rho: ParameterLike;
-    irho: ParameterLike;
-    __class__: "refl1d.material.SLD";
+  name: string;
+  rho: ParameterLike;
+  irho: ParameterLike;
+  __class__: "refl1d.material.SLD";
 }
 
 export interface Magnetism {
-    name: string;
-    rhoM: ParameterLike;
-    thetaM: ParameterLike;
-    phiM: ParameterLike;
-    __class__: "refl1d.material.Magnetism";
+  name: string;
+  rhoM: ParameterLike;
+  thetaM: ParameterLike;
+  phiM: ParameterLike;
+  __class__: "refl1d.material.Magnetism";
 }
 
 export interface Stack {
-    layers: (Slab | Repeat)[];
-    __class__: "refl1d.model.Stack";
+  layers: (Slab | Repeat)[];
+  __class__: "refl1d.model.Stack";
 }
 
 export interface Experiment {
-    sample: Stack;
-    probe: QProbe;
-    __class__: "refl1d.experiment.Experiment";
+  sample: Stack;
+  probe: QProbe;
+  __class__: "refl1d.models.experiment.Experiment";
 }
 
 export interface NumpyArray {
-    values: number[];
-    dtype: string;
-    __class__: "bumps.util.NumpyArray";
+  values: number[];
+  dtype: string;
+  __class__: "bumps.util.NumpyArray";
 }
 
 export interface QProbe {
-    Q: NumpyArray;
-    dQ: NumpyArray;
-    name?: string;
-    filename?: string;
-    intensity: ParameterLike;
-    back_absorption: ParameterLike;
-    background: ParameterLike;
-    back_reflectivity: boolean;
-    R?: NumpyArray;
-    dR?: NumpyArray;
-    resolution: "normal" | "uniform";
-    __class__: "refl1d.probe.QProbe";
+  Q: NumpyArray;
+  dQ: NumpyArray;
+  name?: string;
+  filename?: string;
+  intensity: ParameterLike;
+  back_absorption: ParameterLike;
+  background: ParameterLike;
+  back_reflectivity: boolean;
+  R?: NumpyArray;
+  dR?: NumpyArray;
+  resolution: "normal" | "uniform";
+  __class__: "refl1d.models.probe.probe.QProbe";
 }
