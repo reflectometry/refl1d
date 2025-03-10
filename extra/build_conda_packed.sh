8b93fc05d455269dd8ace81dd00f00b2777f8727
Brian Benjamin Maranville <brian.maranville@nist.gov>
2025-03-10 16:05:14 -0400

    simplify conda-pack step (just do conda-pack)

--- extra/build_conda_packed.sh
+++ extra/build_conda_packed.sh
@@ -1,78 +1,42 @@
 #!/bin/bash
 
+# Definitions
 ENV_NAME="isolated-base"
-PYTHON_VERSION="3.11"
-PKGNAME="refl1d"
-SUBNAME="packed"
-OUTPUT="artifacts"
+PYTHON_VERSION="3.12"
+OUTPUT="conda_packed"
 SCRIPT_DIR=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 SRC_DIR=$(dirname "$SCRIPT_DIR")
-cd $SRC_DIR
-
-mkdir -p $OUTPUT
+pkgdir="$SRC_DIR/$OUTPUT"
 
 eval "$(conda shell.bash hook)"
-conda activate base || { echo 'failed: conda not installed'; exit 1; }
 
-conda install -y conda-pack
 if ! test -f "$ENV_NAME.tar.gz"; then
+  conda install -y conda-pack
   echo "creating isolated environment"
   conda remove -n "$ENV_NAME" -y --all
-  conda create -n "$ENV_NAME" -q --force -y "python=$PYTHON_VERSION" "nodejs"
+  conda create -n "$ENV_NAME" -q --force -y "python=$PYTHON_VERSION" "nodejs" "micromamba"
   conda-pack -n "$ENV_NAME" -f -o "$ENV_NAME.tar.gz"
 fi
 
 # unpack the new environment, that contains only python + pip
-tmpdir=$(mktemp -d)
-pkgdir="$tmpdir/$PKGNAME"
-envdir="$pkgdir/env"
-mkdir -p "$envdir"
-tar -xzf "$ENV_NAME.tar.gz" -C "$envdir"
+# first, clean out the packed folder:
+rm -rf "$pkgdir"
+mkdir -p "$pkgdir"
+tar -xzf "$ENV_NAME.tar.gz" -C "$pkgdir"
 
-# activate the unpacked environment and install pip packages
-# add our batch script:
-case $OSTYPE in 
-  darwin*) cp -r ./extra/platform_scripts/refl1d_webview.app "$pkgdir" ;
-           cp -r ./extra/platform_scripts/refl1d_shell.app "$pkgdir" ;;
-  msys*) cp ./extra/platform_scripts/refl1d_webview.bat "$pkgdir" ;
-         cp ./extra/platform_scripts/refl1d_shell.bat "$pkgdir" ;;
-  linux*) cp -r ./extra/platform_scripts/make_linux_desktop_shortcut.sh "$pkgdir" ;
-          cp -r ./extra/platform_scripts/refl1d-webview "$pkgdir" ;;
-esac
-
-case "$OSTYPE" in
- "msys") bindir=$envdir ;
-         platform="Windows";;
- *) bindir=$envdir/bin ;
-    platform="$(uname -s)";;
-esac
-
+# add any icons
 mkdir -p $pkgdir/share/icons
 cp $SCRIPT_DIR/*.svg $pkgdir/share/icons
 cp $SCRIPT_DIR/*.png $pkgdir/share/icons
+cp $SCRIPT_DIR/*.ico $pkgdir/share/icons
+cp $SCRIPT_DIR/*.icns $pkgdir/share/icons
 
-# install the packages
-$bindir/python -m pip install --no-input --no-compile numba
-# base path to source is in parent of SCRIPT_DIR
-$bindir/python -m pip install --no-input --no-compile .[webview]
-$bindir/python -m pip install orsopy
-
-cd $tmpdir
-# build the client
-$bindir/python -m refl1d.webview.build_client --cleanup
-
-version=$($bindir/python -c "import refl1d; print(refl1d.__version__)")
-mv "$tmpdir/$PKGNAME" "$tmpdir/$PKGNAME-$version"
-
-case $OSTYPE in 
-  # darwin*) cd $tmpdir && hdiutil create -srcfolder  "$PKGNAME-$version" -volname "Refl1D_Jupyter" "$SRC_DIR/Refl1D_Jupyter.dmg" ;; 
-  darwin*) pkgbuild --root $tmpdir --identifier org.reflectometry.$PKGNAME-$SUBNAME --version $version --ownership preserve --install-location /Applications "$SRC_DIR/$OUTPUT/$PKGNAME-$SUBNAME-$version-$platform-$(uname -m).pkg" ;;
-  msys*) conda install -y 7zip ;
-         curl -L https://www.7-zip.org/a/7z2106-x64.exe --output 7z_exe ;
-         7z e 7z_exe -aoa 7z.sfx ;
-         7z a -mhe=on -mx=1 -sfx".\7z.sfx" "$SRC_DIR/$OUTPUT/$PKGNAME-$SUBNAME-$version-$platform-$(uname -m)-self-extracting.exe" "$PKGNAME-$version" ;;
-esac
-
-cd $tmpdir && tar -czf "$SRC_DIR/$OUTPUT/$PKGNAME-$SUBNAME-$version-$platform-$(uname -m).tar.gz" "$PKGNAME-$version"
 cd $SRC_DIR
-rm -rf $tmpdir
+conda activate $pkgdir
+python -m pip install --no-input --no-compile .[webview]
+
+# build the client
+cd $pkgdir
+python -m refl1d.webview.build_client --cleanup
+
+# done
