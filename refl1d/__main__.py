dbb0f5ead2204e742d3210326ae27934ae4f1c7d
Paul Kienzle <pkienzle@nist.gov>
2025-04-02 16:05:27 -0400

    update for new bumps plugin api

---
+++ refl1d/__main__.py
@@ -0,0 +1,11 @@
+"""
+Bumps application.
+
+Run "bumps --help" for details, or "python -m bumps -help" if
+bumps isn't on your path.
+"""
+
+from refl1d.webview.server.cli import main
+
+if __name__ == "__main__":
+    main()
--- refl1d/webview/server/names.py
+++ refl1d/webview/server/names.py
@@ -5,4 +5,4 @@
 from bumps.webview.server.api import state, set_problem, load_session
 from bumps.webview.server.webserver import start_app, create_server_task, sio
 
-from .webserver import main
+from .cli import main
