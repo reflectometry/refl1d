190409fc4fe7d5ffe16f3c5041ab449fbdea27aa
Glass <glass.ships@outlook.com>
2025-04-22 11:36:19 -0400

    update eslint config

--- refl1d/webview/client/eslint.config.js
+++ refl1d/webview/client/eslint.config.js
@@ -1,24 +1,23 @@
 import url from "url";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-// import pluginVue from "eslint-plugin-vue";
+import pluginVue from "eslint-plugin-vue";
 import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
 import prettierConfig from "@vue/eslint-config-prettier";
-import vueTsEslintConfig from "@vue/eslint-config-typescript";
+import { defineConfigWithVueTs, vueTsConfigs } from "@vue/eslint-config-typescript";
 
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
-  // allConfig: js.configs.all,
 });
 
-export default [
+export default defineConfigWithVueTs([
   /** Extend recommended configs */
   ...compat.extends("plugin:vue/vue3-recommended", "plugin:vuejs-accessibility/recommended", "prettier"),
-  // ...pluginVue.configs["flat/recommended"],
-  ...vueTsEslintConfig(),
+  ...pluginVue.configs["flat/recommended"],
   eslintPluginPrettierRecommended,
+  vueTsConfigs.recommended,
   prettierConfig,
   /** Configuration */
   {
@@ -57,4 +56,4 @@ export default [
       ],
     },
   },
-];
+]);
