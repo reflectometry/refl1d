503c7c90008d0d2353ac4fcd421592a5a881742b
bbm <brian.maranville@nist.gov>
2025-05-16 09:20:10 -0400

    probe.calc_Qo cached value is now probe._calc_Q

--- refl1d/webview/server/api.py
+++ refl1d/webview/server/api.py
@@ -76,7 +76,7 @@ async def get_profile_plots(model_specs: List[ModelSpec]):
 
 def get_single_probe_data(theory, probe, substrate=None, surface=None, polarization=""):
     fresnel_calculator = probe.fresnel(substrate, surface)
-    Q, FQ = probe.apply_beam(probe.calc_Q, fresnel_calculator(probe.calc_Qo))
+    Q, FQ = probe.apply_beam(probe.calc_Q, fresnel_calculator(probe._calc_Q))
     Q, R = theory
     output: Dict[str, Union[str, np.ndarray]]
     assert isinstance(FQ, np.ndarray)
