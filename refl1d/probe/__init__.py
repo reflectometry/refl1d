a013980dd65109ac8cae6965a61eb6a89ace6c5d
bbm <brian.maranville@nist.gov>
2025-08-28 09:20:04 -0400

    add OversampledRegion to public namespace

--- refl1d/probe/__init__.py
+++ refl1d/probe/__init__.py
@@ -1,1 +1,11 @@
-from .probe import Probe, XrayProbe, NeutronProbe, PolarizedNeutronProbe, QProbe, PolarizedQProbe, ProbeSet, make_probe
+from .probe import (
+    Probe,
+    XrayProbe,
+    NeutronProbe,
+    OversampledRegion,
+    PolarizedNeutronProbe,
+    QProbe,
+    PolarizedQProbe,
+    ProbeSet,
+    make_probe,
+)
